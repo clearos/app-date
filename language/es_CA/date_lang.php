8fce61bd9f598564c3720ee546e59736cb3e0d70
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-11-08 16:41:19 +0000

    - Updated translations

--- language/es_CA/date_lang.php
+++ language/es_CA/date_lang.php
@@ -3,14 +3,18 @@
 // http://www.clearfoundation.com/docs/developer/translations/
 
 $lang['date_app_description'] = 'Eina de zona horaria i sincronització del rellotge.';
-$lang['date_app_name'] = 'Hora i Data';
+$lang['date_app_name'] = 'Date and Time';
 $lang['date_app_tooltip'] = 'VoIP, serveis d\'autenticació i altres aplicacions requereixen una configuració adequada del temps, així que si us plau assegureu-vos que el rellotge i la zona horaria són correctes.';
 $lang['date_date'] = 'Data';
-$lang['date_synchronization_changed_time_by_x_seconds:'] = 'La sincronització per canviar l\'hora del sistema en el següent nombre de segons:';
+$lang['date_synchronize'] = 'Synchronize';
+$lang['date_synchronize_help'] = 'Click on the synchronize button to update the system clock with the correct time.  Your system will automatically synchronize its clock from time to time.';
 $lang['date_synchronize_now'] = 'Sincronitza Ara';
+$lang['date_synchronized'] = 'Synchronized';
+$lang['date_synchronizing'] = 'Synchronizing';
 $lang['date_time'] = 'Hora';
 $lang['date_time_server'] = 'Servidor d\'Hora';
 $lang['date_time_server_is_invalid'] = 'El servidor d\'hora és incorrecte';
 $lang['date_time_synchronization_schedule_is_invalid'] = 'El temps de sincronització de l\'horari no es vàlid';
 $lang['date_time_zone'] = 'Zona Horària';
 $lang['date_time_zone_is_invalid'] = 'La zona horària és incorrecte';
+$lang['date_time_zone_wizard_help'] = 'Many apps and services require correct time zone information and an accurate clock, so it is important that it is set correctly.';

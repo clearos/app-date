df64ec4d368ca3c859fc2955c4628213e0a0a4a4
pcbaldwin <pbaldwin@clearfoundation.com>
2014-05-08 20:36:56 -0400

    Prepped for build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'date';
-$app['version'] = '1.5.0';
+$app['version'] = '1.6.0';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-date.spec
+++ packaging/app-date.spec
@@ -1,7 +1,7 @@
 
 Name: app-date
 Epoch: 1
-Version: 1.5.0
+Version: 1.6.0
 Release: 1%{dist}
 Summary: Date and Time
 License: GPLv3

22ccfbd94637d3e58611e3fb3ff4ec25fc621ed4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-07 22:22:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'La zona horaria y la herramienta de sincronización de reloj.';
+$lang['date_app_name'] = 'Fecha y hora';
+$lang['date_app_tooltip'] = 'VoIP, servicios de autenticación y otras aplicaciones requieren ajustes de tiempo apropiados, así que por favor asegúrese de que su reloj y zona horaria son correctas!';
+$lang['date_automatic_synchronize'] = 'Sincronizar automática';
+$lang['date_date'] = 'Fecha';
+$lang['date_synchronize'] = 'Sincronizar';
+$lang['date_synchronize_now'] = 'sincronizar Ahora';
+$lang['date_synchronize_wizard_tip'] = 'Con sincronización automática activada, el sistema sincronizar su reloj de vez en cuando.';
+$lang['date_synchronized'] = 'sincronizada';
+$lang['date_synchronizing'] = 'sincronización';
+$lang['date_time'] = 'Hora';
+$lang['date_time_server'] = 'El servidor de hora';
+$lang['date_time_server_is_invalid'] = 'servidor de tiempo no es válido.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'programa de sincronización de tiempo no es válido.';
+$lang['date_time_zone'] = 'Zona horaria';
+$lang['date_time_zone_is_invalid'] = 'zona horaria no es válido.';
+$lang['date_time_zone_wizard_help'] = 'Muchas aplicaciones y servicios requieren información de zona horaria correcta y un reloj de precisión, por lo que es importante que se establece correctamente.';

2befaf42161a7e6afa209b2001a8c4c9d3e9b829
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-17 23:41:00 +0000

    Add new files and directories, commit changes, push changes

---
+++ language/ur/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'ٹائم زون اور گھڑی ہم وقت سازی کے آلے.';
+$lang['date_app_name'] = 'تاریخ اور وقت';
+$lang['date_app_tooltip'] = 'ویوآئپی، تصدیق خدمات اور دوسرے پروگراموں، مناسب وقت کی ترتیبات کی ضرورت ہوتی ہے تو براہ مہربانی اس بات کا یقین آپ کی گھڑی اور ٹائم زون درست ہیں بنانے کے لئے!';
+$lang['date_automatic_synchronize'] = 'خود کار ہم وقت';
+$lang['date_date'] = 'تاریخ';
+$lang['date_synchronize'] = 'ہم وقت سازی';
+$lang['date_synchronize_now'] = 'ابھی ہم آہنگ کریں';
+$lang['date_synchronize_wizard_tip'] = 'خود کار طریقے سے مطابقت فعال کے ساتھ، نظام وقت سے وقت کے لئے اس گھڑی مطابقت گا.';
+$lang['date_synchronized'] = 'آہنگ';
+$lang['date_synchronizing'] = 'ہم وقت سازی';
+$lang['date_time'] = 'وقت';
+$lang['date_time_server'] = 'وقت سرور';
+$lang['date_time_server_is_invalid'] = 'وقت سرور غلط ہے.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'وقت ہم آہنگی کے شیڈول غلط ہے.';
+$lang['date_time_zone'] = 'ٹائم زون';
+$lang['date_time_zone_is_invalid'] = 'ٹائم زون غلط ہے.';
+$lang['date_time_zone_wizard_help'] = 'کئی اطلاقات اور خدمات درست ٹائم زون کی معلومات اور ایک درست گھڑی کی ضرورت ہوتی ہے، تو یہ اس صحیح طریقے سے قائم کیا جاتا ہے کہ اہم ہے.';

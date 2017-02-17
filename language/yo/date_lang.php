89656e5787587475698823e27d81d88b7cb6dde9
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-17 23:21:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Aago agbegbe ati clock amuṣiṣẹpọ ọpa.';
+$lang['date_app_name'] = 'Ọjọ ati Time';
+$lang['date_app_tooltip'] = 'Voip, ìfàṣẹsí awọn iṣẹ ati awọn ohun elo miiran beere dara akoko eto, ki jọwọ rii daju rẹ aago ati akoko ibi wa ti o tọ!';
+$lang['date_automatic_synchronize'] = 'laifọwọyi ṣiṣepo';
+$lang['date_date'] = 'ọjọ';
+$lang['date_synchronize'] = 'muu';
+$lang['date_synchronize_now'] = 'muu Bayi';
+$lang['date_synchronize_wizard_tip'] = 'Pẹlu laifọwọyi Muu ṣiṣẹ, awọn eto yoo muu awọn oniwe-aago lati akoko si akoko.';
+$lang['date_synchronized'] = 'šišẹpọ';
+$lang['date_synchronizing'] = 'mimušišẹpọ';
+$lang['date_time'] = 'Time';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'Time olupin ni invalid.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Time amuṣiṣẹpọ iṣeto ni invalid.';
+$lang['date_time_zone'] = 'Time Zone';
+$lang['date_time_zone_is_invalid'] = 'Agbegbe aago ni invalid.';
+$lang['date_time_zone_wizard_help'] = 'Ọpọlọpọ awọn apps ati awọn iṣẹ beere ti o tọ aago alaye ati ohun deede aago, ki o jẹ pataki ki o ti wa ni ṣeto bi o ti tọ.';

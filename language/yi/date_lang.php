5ecce3d259b92c5c50cc2fcb0d845e6ecab4a7c5
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-17 23:23:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'די צייַט זאָנע און זייגער סינגקראַנאַזיישאַן געצייַג.';
+$lang['date_app_name'] = 'דאטום און צייט';
+$lang['date_app_tooltip'] = 'וואָיפּ, אָטענטאַקיישאַן באַדינונגען און אנדערע אַפּלאַקיישאַנז דאַרפן געהעריק צייַט סעטטינגס, אַזוי ביטע מאַכן זיכער דיין זייגער און צייַט זאָנע זענען ריכטיק!';
+$lang['date_automatic_synchronize'] = 'אָטאַמאַטיק סינטשראָניזע';
+$lang['date_date'] = 'דאַטע';
+$lang['date_synchronize'] = 'סינטשראָניזע';
+$lang['date_synchronize_now'] = 'סינטשראָניזע איצט';
+$lang['date_synchronize_wizard_tip'] = 'מיט אָטאַמאַטיק סינגקראַנייז ענייבאַלד, די סיסטעם וועט סינגקראַנייז זייַן זייגער פֿון מאָל צו מאָל.';
+$lang['date_synchronized'] = 'סינגקראַנייזד';
+$lang['date_synchronizing'] = 'סינגקראַנייזינג';
+$lang['date_time'] = 'צייַט';
+$lang['date_time_server'] = 'צייַט סערווירער';
+$lang['date_time_server_is_invalid'] = 'צייַט סערווער איז פאַרקריפּלט.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'צייַט סינגקראַנאַזיישאַן פּלאַן איז פאַרקריפּלט.';
+$lang['date_time_zone'] = 'צייַט זאָנע';
+$lang['date_time_zone_is_invalid'] = 'צייט זאנע איז פאַרקריפּלט.';
+$lang['date_time_zone_wizard_help'] = 'פילע אַפּס און סערוויסעס דאַרפן ריכטיק זאָנע אינפֿאָרמאַציע און אַן פּינטלעך זייגער, אַזוי עס איז וויכטיק אַז עס איז באַשטימט ריכטיק.';

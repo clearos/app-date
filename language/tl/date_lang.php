f64f03ef390c01697fcdfbbc2690d9331a516824
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-17 23:54:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Ang time zone at orasan synchronization kasangkapan.';
+$lang['date_app_name'] = 'Petsa at oras';
+$lang['date_app_tooltip'] = 'VoIP, authentication serbisyo at iba pang mga aplikasyon ay nangangailangan ng tamang setting ng oras, kaya&#39;t mangyaring siguraduhin na ang iyong orasan at time zone ay tama!';
+$lang['date_automatic_synchronize'] = 'Awtomatikong I-synchronize';
+$lang['date_date'] = 'petsa';
+$lang['date_synchronize'] = 'I-synchronize ang';
+$lang['date_synchronize_now'] = 'I-synchronize Ngayon';
+$lang['date_synchronize_wizard_tip'] = 'Kapag pinagana ang awtomatikong synchronize, ang sistema ay i-synchronize ang kanyang orasan sa pana-panahon.';
+$lang['date_synchronized'] = 'Naka-sync';
+$lang['date_synchronizing'] = 'Synchronizing';
+$lang['date_time'] = 'oras';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'Time server ay hindi wasto.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Time synchronization iskedyul ay hindi wasto.';
+$lang['date_time_zone'] = 'Time Zone';
+$lang['date_time_zone_is_invalid'] = 'Time zone ay hindi wasto.';
+$lang['date_time_zone_wizard_help'] = 'Maraming mga apps at mga serbisyo ay nangangailangan ng tamang impormasyon time zone at ng isang tumpak na orasan, sa gayon ito ay mahalaga na ito ay naka-set ng tama.';

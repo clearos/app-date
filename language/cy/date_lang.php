a3603c68b8212af9e67028c2220febc6bb5b6e9a
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-17 23:26:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Mae'r parth amser ac offeryn synchronization cloc.';
+$lang['date_app_name'] = 'Dyddiad ac Amser';
+$lang['date_app_tooltip'] = 'VoIP, gwasanaethau dilysu a cheisiadau eraill yn gofyn lleoliadau amser priodol, felly gwnewch yn siŵr eich cloc a parth amser yn gywir!';
+$lang['date_automatic_synchronize'] = 'Awtomatig Cydamseru';
+$lang['date_date'] = 'dyddiad';
+$lang['date_synchronize'] = 'cydamseru';
+$lang['date_synchronize_now'] = 'cydamseru Now';
+$lang['date_synchronize_wizard_tip'] = 'Gyda Cydamseru awtomatig alluogi, bydd y system yn cydweddu ei gloc o bryd i&#39;w gilydd.';
+$lang['date_synchronized'] = 'cydamserol';
+$lang['date_synchronizing'] = 'Cysoni';
+$lang['date_time'] = 'amser';
+$lang['date_time_server'] = 'amser Gweinydd';
+$lang['date_time_server_is_invalid'] = 'Amser gweinydd yn annilys.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'amserlen amser synchronization yn annilys.';
+$lang['date_time_zone'] = 'Parth amser';
+$lang['date_time_zone_is_invalid'] = 'parth amser yn annilys.';
+$lang['date_time_zone_wizard_help'] = 'Mae llawer o apps a gwasanaethau angen gwybodaeth parth amser cywir ac gloc cywir, felly mae&#39;n bwysig bod yn cael ei osod yn gywir.';

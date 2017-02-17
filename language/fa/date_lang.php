8a76302d2a96b9ac39cac58379050cd4eb442f34
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-17 22:17:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'منطقه زمان و ابزار هماهنگ سازی ساعت.';
+$lang['date_app_name'] = 'تاریخ و زمان';
+$lang['date_app_tooltip'] = 'از VoIP، خدمات احراز هویت و برنامه های کاربردی دیگر نیاز به تنظیمات زمان مناسب، پس لطفا مطمئن شوید ساعت و منطقه زمانی خود درست باشد!';
+$lang['date_automatic_synchronize'] = 'همگام سازی خودکار';
+$lang['date_date'] = 'تاریخ';
+$lang['date_synchronize'] = 'همگام سازی';
+$lang['date_synchronize_now'] = 'همگام سازی در حال حاضر';
+$lang['date_synchronize_wizard_tip'] = 'با همگام سازی خودکار را فعال کنید، سیستم ساعت خود را از زمان به زمان همگام سازی کنید.';
+$lang['date_synchronized'] = 'همگام شده';
+$lang['date_synchronizing'] = 'همگام سازی';
+$lang['date_time'] = 'زمان';
+$lang['date_time_server'] = 'ساعت سرور';
+$lang['date_time_server_is_invalid'] = 'سرور هم نامعتبر است.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'برنامه هماهنگ سازی زمان نامعتبر است.';
+$lang['date_time_zone'] = 'منطقه زمانی';
+$lang['date_time_zone_is_invalid'] = 'منطقه زمانی نامعتبر است.';
+$lang['date_time_zone_wizard_help'] = 'بسیاری از برنامه ها و خدمات نیاز به اطلاعات منطقه زمانی صحیح و یک ساعت دقیق است، بنابراین مهم است که آن را به درستی تنظیم شده است.';

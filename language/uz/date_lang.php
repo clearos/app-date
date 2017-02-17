77c0cb9de76e50cc0fee4b022dfd5ba902e0e414
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-17 23:37:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Vaqt mintaqasi va soat sinxronlash vositasi.';
+$lang['date_app_name'] = 'Sana va vaqt';
+$lang['date_app_tooltip'] = 'VoIP, autentifikatsiya xizmatlar va boshqa dasturlar shuning uchun soat va vaqt mintaqasi to&#39;g&#39;ri ishonch hosil qiling, to&#39;g&#39;ri vaqt moslamalarini talab!';
+$lang['date_automatic_synchronize'] = 'Avtomatik Sinxronizatsiya';
+$lang['date_date'] = 'Sana';
+$lang['date_synchronize'] = 'sinxronlashtirish';
+$lang['date_synchronize_now'] = 'Endi sinxronlashtirish';
+$lang['date_synchronize_wizard_tip'] = 'avtomatik sinxronlash yoqilgan bilan, tizim vaqti-vaqti bilan o&#39;z soat sinxronlashtirish bo&#39;ladi.';
+$lang['date_synchronized'] = 'sinxron';
+$lang['date_synchronizing'] = 'Sinxronizatsiya';
+$lang['date_time'] = 'vaqt';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'Vaqt server haqiqiy emas.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Time sinxronizatsiya jadvali haqiqiy emas.';
+$lang['date_time_zone'] = 'Vaqt zonasi';
+$lang['date_time_zone_is_invalid'] = 'Vaqt zonasi haqiqiy emas.';
+$lang['date_time_zone_wizard_help'] = 'Ko&#39;pchilik ilovalar va xizmatlar to&#39;g&#39;ri soat mintaqasi ma&#39;lumotlarini va aniq soat talab, shuning uchun u to&#39;g&#39;ri o&#39;rnatilgan bo&#39;lishi muhim ahamiyatga ega.';

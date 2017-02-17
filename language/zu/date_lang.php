00d3cf118f1e2eff9df68cea8b3e9d6050b92e8d
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-17 23:19:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Umkhawuko wesikhathi futhi ithuluzi clock ukuvumelanisa.';
+$lang['date_app_name'] = 'Usuku nesikhathi';
+$lang['date_app_tooltip'] = 'VoIP, amasevisi ukuqinisekiswa kanye nezinye zidinga izilungiselelo efanele ngesikhathi, ngakho ke sicela uqiniseke iwashi lakho kanye nesikhathi zone zilungile!';
+$lang['date_automatic_synchronize'] = 'Automatic Vumelanisa';
+$lang['date_date'] = 'Usuku';
+$lang['date_synchronize'] = 'Vumelanisa';
+$lang['date_synchronize_now'] = 'Vumelanisa Manje';
+$lang['date_synchronize_wizard_tip'] = 'With vumelanisa othomathikhi inikwe amandla, ohlelweni ukuvumelanisa iwashi layo ngezikhathi ezithile.';
+$lang['date_synchronized'] = 'Kuxhunyaniswé';
+$lang['date_synchronizing'] = 'Kuyavumelaniswa';
+$lang['date_time'] = 'Isikhathi';
+$lang['date_time_server'] = 'Isikhathi Server';
+$lang['date_time_server_is_invalid'] = 'iseva Isikhathi ayivumelekile.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Isikhathi ukuvumelanisa isimiso ayivumelekile.';
+$lang['date_time_zone'] = 'Isikhathi Zone';
+$lang['date_time_zone_is_invalid'] = 'Isikhathi zone ayivumelekile.';
+$lang['date_time_zone_wizard_help'] = 'Iningi lezinhlelo zokusebenza kanye namasevisi adinga letifanele time zone kanye newashi olunembile, ngakho kubalulekile ukuthi isethwe ngendlela efanele.';

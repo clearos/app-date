4b33e34a3880d24cf8b45485207b173afe4ae6ff
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-17 23:57:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'เครื่องมือโซนเวลาและการประสานนาฬิกา';
+$lang['date_app_name'] = 'วันและเวลา';
+$lang['date_app_tooltip'] = 'VoIP, บริการตรวจสอบและการใช้งานอื่น ๆ ต้องใช้การตั้งค่าเวลาที่เหมาะสมดังนั้นโปรดตรวจสอบให้แน่ใจนาฬิกาและโซนเวลาของคุณถูกต้อง!';
+$lang['date_automatic_synchronize'] = 'ประสานอัตโนมัติ';
+$lang['date_date'] = 'วันที่';
+$lang['date_synchronize'] = 'ประสาน';
+$lang['date_synchronize_now'] = 'ประสานตอนนี้';
+$lang['date_synchronize_wizard_tip'] = 'ด้วยการประสานอัตโนมัติเปิดใช้งานระบบจะประสานนาฬิกาเป็นครั้งคราว';
+$lang['date_synchronized'] = 'Synchronized';
+$lang['date_synchronizing'] = 'ตรงกัน';
+$lang['date_time'] = 'เวลา';
+$lang['date_time_server'] = 'เวลาของเซิร์ฟเวอร์';
+$lang['date_time_server_is_invalid'] = 'เซิร์ฟเวอร์เวลาไม่ถูกต้อง';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'ตารางการประสานเวลาไม่ถูกต้อง';
+$lang['date_time_zone'] = 'เขตเวลา';
+$lang['date_time_zone_is_invalid'] = 'โซนเวลาไม่ถูกต้อง';
+$lang['date_time_zone_wizard_help'] = 'ปพลิเคชันและบริการจำนวนมากต้องการข้อมูลโซนเวลาที่ถูกต้องและเป็นนาฬิกาที่ถูกต้องจึงเป็นสิ่งสำคัญที่จะตั้งอย่างถูกต้อง';

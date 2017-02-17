3e2a6afe70fddcf769cc41cee0e15353d8edf120
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-17 23:47:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'zaman dilimi ve saat senkronizasyon aracı.';
+$lang['date_app_name'] = 'Tarih ve saat';
+$lang['date_app_tooltip'] = 'VoIP, kimlik doğrulama hizmetleri ve diğer uygulamalar böylece saat ve saat dilimi doğru olduğundan emin olun, doğru zaman ayarları gerektirir!';
+$lang['date_automatic_synchronize'] = 'otomatik Senkronize';
+$lang['date_date'] = 'tarih';
+$lang['date_synchronize'] = 'senkronize etmek';
+$lang['date_synchronize_now'] = 'Şimdi senkronize';
+$lang['date_synchronize_wizard_tip'] = 'Otomatik senkronize etkinleştirildiğinde, sistem zaman zaman kendi saatini senkronize edecektir.';
+$lang['date_synchronized'] = 'senkronize';
+$lang['date_synchronizing'] = 'Senkronizasyon';
+$lang['date_time'] = 'Zaman';
+$lang['date_time_server'] = 'Zaman Sunucusu';
+$lang['date_time_server_is_invalid'] = 'Zaman sunucusu geçersiz.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Zaman senkronizasyonu programı geçersiz.';
+$lang['date_time_zone'] = 'Saat dilimi';
+$lang['date_time_zone_is_invalid'] = 'Zaman dilimi geçersiz.';
+$lang['date_time_zone_wizard_help'] = 'Birçok uygulamalar ve hizmetler doğru saat dilimi bilgilerini ve doğru saati gerektirir, yüzden doğru ayarlanmış olması önemlidir.';

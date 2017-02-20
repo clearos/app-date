0efc6359bf18c146a291a56e4579fb248ac5de91
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 18:40:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Zon masa dan alat penyegerakan jam.';
+$lang['date_app_name'] = 'Tarikh dan masa';
+$lang['date_app_tooltip'] = 'VoIP, perkhidmatan pengesahan dan aplikasi lain memerlukan tetapan masa yang tetap, jadi sila pastikan jam anda dan zon masa adalah betul!';
+$lang['date_automatic_synchronize'] = 'automatik Menyegerak';
+$lang['date_date'] = 'Tarikh';
+$lang['date_synchronize'] = 'Segerakkan';
+$lang['date_synchronize_now'] = 'Segerakkan sekarang';
+$lang['date_synchronize_wizard_tip'] = 'Dengan menyegerakkan automatik diaktifkan, sistem akan menyegerakkan jam dari semasa ke semasa.';
+$lang['date_synchronized'] = 'Synchronized';
+$lang['date_synchronizing'] = 'Penyelarasan';
+$lang['date_time'] = 'Masa';
+$lang['date_time_server'] = 'masa Server';
+$lang['date_time_server_is_invalid'] = 'pelayan masa adalah tidak sah.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'jadual penyegerakan masa adalah tidak sah.';
+$lang['date_time_zone'] = 'Zon masa';
+$lang['date_time_zone_is_invalid'] = 'zon masa adalah tidak sah.';
+$lang['date_time_zone_wizard_help'] = 'Banyak aplikasi dan perkhidmatan memerlukan maklumat zon masa yang betul dan jam tepat, jadi ia adalah penting bahawa ia ditetapkan dengan betul.';

1bb15687858625814f2704ad9c50c96a337aec68
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 17:14:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Strefa czasowa i narzędzie do synchronizacji zegara.';
+$lang['date_app_name'] = 'Data i godzina';
+$lang['date_app_tooltip'] = 'VoIP, usługi uwierzytelniania i inne aplikacje wymagają odpowiedniego ustawienia czasu, więc upewnij się, że zegar i strefa czasowa są poprawne!';
+$lang['date_automatic_synchronize'] = 'Automatyczna synchronizacja';
+$lang['date_date'] = 'Data';
+$lang['date_synchronize'] = 'Synchronizować';
+$lang['date_synchronize_now'] = 'Synchronizuj teraz';
+$lang['date_synchronize_wizard_tip'] = 'Przy włączonej funkcji automatycznej synchronizacji system synchronizuje swój zegar od czasu do czasu.';
+$lang['date_synchronized'] = 'zsynchronizowane';
+$lang['date_synchronizing'] = 'Synchronizacja';
+$lang['date_time'] = 'Czas';
+$lang['date_time_server'] = 'Serwer czasu';
+$lang['date_time_server_is_invalid'] = 'Serwer czasu jest nieprawidłowy.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Harmonogram synchronizacja czasu jest nieprawidłowa.';
+$lang['date_time_zone'] = 'Strefa czasowa';
+$lang['date_time_zone_is_invalid'] = 'Strefa czasowa jest nieprawidłowy.';
+$lang['date_time_zone_wizard_help'] = 'Wiele aplikacji i usług wymaga poprawnej informacji strefę czasową i dokładny zegar, więc ważne jest to, że jest prawidłowo ustawiony.';

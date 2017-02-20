81c531fedcc9d3893b2c1f87feb9fd1887b84f26
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 16:38:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'நேர மண்டலம் மற்றும் கடிகாரம் ஒருங்கிணைப்பு கருவி.';
+$lang['date_app_name'] = 'தேதி மற்றும் நேரம்';
+$lang['date_app_tooltip'] = 'VoIP ஐ, அங்கீகார சேவைகள் மற்றும் பிற பயன்பாடுகளில், உரிய நேரத்தில் அமைப்புகள் தேவைப்படும் உங்கள் கடிகாரம் மற்றும் நேர மண்டலம் சரியாக உறுதி செய்யவும்!';
+$lang['date_automatic_synchronize'] = 'தானியங்கி ஒருங்கிணைக்கவும்';
+$lang['date_date'] = 'தேதி';
+$lang['date_synchronize'] = 'ஒத்திசை';
+$lang['date_synchronize_now'] = 'இப்போது ஒத்திசை';
+$lang['date_synchronize_wizard_tip'] = 'தானியங்கி ஒத்திசை செயல்படுத்தப்படும், அமைப்பு அவ்வப்போது அதன் கடிகார ஒருங்கிணைக்கிறது.';
+$lang['date_synchronized'] = 'ஒரே சமயத்தில் நடைபெறும்';
+$lang['date_synchronizing'] = 'ஒருங்கிணைத்தல்';
+$lang['date_time'] = 'நேரம்';
+$lang['date_time_server'] = 'நேரம் சர்வர்';
+$lang['date_time_server_is_invalid'] = 'நேரம் சர்வர் தவறானது.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'நேரம் ஒருங்கிணைக்க திட்டமிடப்படும் தவறானது.';
+$lang['date_time_zone'] = 'நேரம் மண்டலம்';
+$lang['date_time_zone_is_invalid'] = 'நேர வலயம் தவறானது.';
+$lang['date_time_zone_wizard_help'] = 'பல பயன்பாடுகள் மற்றும் சேவைகள் சரியான நேர மண்டலத்தை தகவல் மற்றும் ஒரு துல்லியமான கடிகாரம் தேவைப்படும், அதனால் அதை சரியாக அமைத்து என்று முக்கியம்.';

99ec183f9f3d44b5041d49b2950458efe3041877
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 17:02:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Временска зона и алат за сат синхронизација.';
+$lang['date_app_name'] = 'Датум и време';
+$lang['date_app_tooltip'] = 'ВоИП аутентичности услуге и друге апликације захтевају одговарајуће подешавања времена, тако да проверите да ли ваш сат и временска зона тачни!';
+$lang['date_automatic_synchronize'] = 'automatski Синцхронизе';
+$lang['date_date'] = 'датум';
+$lang['date_synchronize'] = 'синхронизовати';
+$lang['date_synchronize_now'] = 'синхронизовати Сада';
+$lang['date_synchronize_wizard_tip'] = 'Са омогућено аутоматско усклађивање, систем ће ускладити своје сат времена на време.';
+$lang['date_synchronized'] = 'синхронизован';
+$lang['date_synchronizing'] = 'Усклађивање';
+$lang['date_time'] = 'време';
+$lang['date_time_server'] = 'опортуниста';
+$lang['date_time_server_is_invalid'] = 'Тиме сервер је неважећи.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'рокови синхронизација је неважећи.';
+$lang['date_time_zone'] = 'Временска зона';
+$lang['date_time_zone_is_invalid'] = 'Временска зона је неважећи.';
+$lang['date_time_zone_wizard_help'] = 'Многе апликације и услуге захтевају тачне информације о временској зони и тачан сат, тако да је важно да је правилно постављена.';

911bf2f1062be5131e34dd3cd655b43faba48adb
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 22:08:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Az időzónát és órát szinkronizációs eszköz.';
+$lang['date_app_name'] = 'Dátum és idő';
+$lang['date_app_tooltip'] = 'VoIP, hitelesítési szolgáltatások és egyéb alkalmazás igényli a megfelelő idő beállításait, ezért kérjük, ellenőrizze az óra és időzóna helyes!';
+$lang['date_automatic_synchronize'] = 'Automatikus szinkronizálás';
+$lang['date_date'] = 'Dátum';
+$lang['date_synchronize'] = 'szinkronizálása';
+$lang['date_synchronize_now'] = 'Szinkronizálás most';
+$lang['date_synchronize_wizard_tip'] = 'Az automatikus szinkronizálás be van kapcsolva, akkor a rendszer szinkronizálja az óráját időről időre.';
+$lang['date_synchronized'] = 'szinkronizált';
+$lang['date_synchronizing'] = 'Szinkronizálás';
+$lang['date_time'] = 'Idő';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'Időkiszolgáló érvénytelen.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Idő szinkronizálás ütem érvénytelen.';
+$lang['date_time_zone'] = 'Időzóna';
+$lang['date_time_zone_is_invalid'] = 'Időzóna érvénytelen.';
+$lang['date_time_zone_wizard_help'] = 'Számos alkalmazás és szolgáltatásokat igényelnek megfelelő időzónát információkat és a pontos órát, ezért fontos, hogy megfelelően van beállítva.';

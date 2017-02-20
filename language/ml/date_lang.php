49506df2c6010e4f5c6f65698c4c881b3f00bb04
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 18:51:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'സമയ മേഖല ക്ലോക്ക് സമന്വയം ഉപകരണം.';
+$lang['date_app_name'] = 'തീയതിയും സമയവും';
+$lang['date_app_tooltip'] = 'VoIP, ആധികാരികത മറ്റ് അപ്ലിക്കേഷനുകളും യഥാസമയം ക്രമീകരണങ്ങൾ ആവശ്യമായ, അതിനാൽ നിങ്ങളുടെ ക്ലോക്കും സമയ മേഖലയും ശരിയാണെന്ന് ഉറപ്പാക്കുക ദയവായി!';
+$lang['date_automatic_synchronize'] = 'ഓട്ടോമാറ്റിക് സമന്വയിപ്പിക്കുക';
+$lang['date_date'] = 'തീയതി';
+$lang['date_synchronize'] = 'സമന്വയിപ്പിക്കുക';
+$lang['date_synchronize_now'] = 'ഇപ്പോൾ സമന്വയിപ്പിക്കുക';
+$lang['date_synchronize_wizard_tip'] = 'ഓട്ടോമാറ്റിക് സമന്വയിപ്പിക്കുക പ്രാപ്തമാക്കി, സിസ്റ്റം കാലാകാലങ്ങളിൽ അതിന്റെ ക്ലോക്കിലേക്ക് ചെയ്യും.';
+$lang['date_synchronized'] = 'താളാത്മക';
+$lang['date_synchronizing'] = 'ഏകീകരിക്കുന്നു';
+$lang['date_time'] = 'കാലം';
+$lang['date_time_server'] = 'സമയം സെർവർ';
+$lang['date_time_server_is_invalid'] = 'സമയം സെർവർ അസാധുവാണ്.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'സമന്വയം ഷെഡ്യൂൾ അസാധുവാണ്.';
+$lang['date_time_zone'] = 'സമയ മേഖല';
+$lang['date_time_zone_is_invalid'] = 'സമയ മേഖല അസാധുവാണ്.';
+$lang['date_time_zone_wizard_help'] = 'പല അപ്ലിക്കേഷനുകളും സേവനങ്ങളും ശരിയായ സമയ മേഖല വിവരങ്ങളും കൃത്യമായ സമയം ആവശ്യമായ, അങ്ങനെ അത് ശരിയായി സജ്ജീകരിച്ച പ്രധാനമാണ്.';

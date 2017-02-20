ab4eb7cde6ce07ed3ad2ba9ca34bb8bf221fab78
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 22:59:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'La tempozonon kaj horloĝo sincronización ilo.';
+$lang['date_app_name'] = 'Dato kaj Tempo';
+$lang['date_app_tooltip'] = 'VoIP, autenticación servoj kaj aliaj aplikoj postulas konvenan tempon agordojn, do bonvolu certigi vian horloĝon kaj horzono estas korekta!';
+$lang['date_automatic_synchronize'] = 'aŭtomata Sinkronigu';
+$lang['date_date'] = 'Dato';
+$lang['date_synchronize'] = 'sinkronigi';
+$lang['date_synchronize_now'] = 'sinkronigi Nun';
+$lang['date_synchronize_wizard_tip'] = 'Kun aŭtomata Sinkronigu ebligita, la sistemo sinkronigi lian horloĝon de tempo al tempo.';
+$lang['date_synchronized'] = 'sinkronigita';
+$lang['date_synchronizing'] = 'sinkronigante';
+$lang['date_time'] = 'tempo';
+$lang['date_time_server'] = 'tempo Servilo';
+$lang['date_time_server_is_invalid'] = 'Tempo servilo estas nevalida.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Tempo sincronización horaro estas nevalida.';
+$lang['date_time_zone'] = 'Horzono';
+$lang['date_time_zone_is_invalid'] = 'Horzono estas nevalida.';
+$lang['date_time_zone_wizard_help'] = 'Multaj apps kaj servoj postulas ĝentilan horzono informojn kaj precizan horloĝon, do ĝi estas grava ke ĝi estas ĝuste difinita.';

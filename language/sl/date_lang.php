c4231a5d573fdfe79303347cc2d7bbea6ce3cb8c
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 16:55:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Časovni pas in orodje za sinhronizacijo ure.';
+$lang['date_app_name'] = 'Datum in čas';
+$lang['date_app_tooltip'] = 'VoIP, preverjanje pristnosti storitve in druge aplikacije zahtevajo ustrezne časovne nastavitve, zato poskrbite, da vaša ura in časovni pas je pravilen!';
+$lang['date_automatic_synchronize'] = 'Samodejna Sinhronizacija';
+$lang['date_date'] = 'Datum';
+$lang['date_synchronize'] = 'Sinhronizacija';
+$lang['date_synchronize_now'] = 'Sinhronizacija zdaj';
+$lang['date_synchronize_wizard_tip'] = 'S samodejno sinhronizacijo omogočeno, bo sistem sinhronizirati svojo uro od časa do časa.';
+$lang['date_synchronized'] = 'sinhronizirano';
+$lang['date_synchronizing'] = 'Sinhronizacija';
+$lang['date_time'] = 'čas';
+$lang['date_time_server'] = 'čas Server';
+$lang['date_time_server_is_invalid'] = 'Čas strežnika ni veljavna.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Časovna sinhronizacija urnik je neveljavna.';
+$lang['date_time_zone'] = 'Časovni pas';
+$lang['date_time_zone_is_invalid'] = 'Časovni pas je neveljavna.';
+$lang['date_time_zone_wizard_help'] = 'Številne aplikacije in storitve zahtevajo pravilne podatke časovni pas in natančno uro, zato je pomembno, da je pravilno nastavljen.';

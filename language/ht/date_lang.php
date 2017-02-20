a80ea96f07eaba8c0d157206fbf2f7dc61c3218c
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 22:25:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Zòn nan tan ak zouti senkronizasyon revèy.';
+$lang['date_app_name'] = 'Dat ak Lè';
+$lang['date_app_tooltip'] = 'VoIP, sèvis otantifikasyon ak aplikasyon pou lòt mande pou anviwònman tan apwopriye, kidonk, tanpri asire w ke revèy ou ak zòn tan yo kòrèk!';
+$lang['date_automatic_synchronize'] = 'Otomatik Senkroniz';
+$lang['date_date'] = 'Dat';
+$lang['date_synchronize'] = 'senkroniz';
+$lang['date_synchronize_now'] = 'senkroniz Koulye a,';
+$lang['date_synchronize_wizard_tip'] = 'Avèk otomatik Senkroniz pèmèt, sistèm lan pral senkroniz revèy li yo soti nan tan zan tan.';
+$lang['date_synchronized'] = 'senkronize';
+$lang['date_synchronizing'] = 'senkronism';
+$lang['date_time'] = 'Tan';
+$lang['date_time_server'] = 'Tan sèvè';
+$lang['date_time_server_is_invalid'] = 'Tan sèvè se valab.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'orè senkronizasyon Tan se valab.';
+$lang['date_time_zone'] = 'Zòn Tan';
+$lang['date_time_zone_is_invalid'] = 'zòn Tan se valab.';
+$lang['date_time_zone_wizard_help'] = 'Anpil apps ak sèvis mande pou kòrèk enfòmasyon zòn tan ak yon revèy egzat, se konsa li enpòtan pou li se mete kòrèkteman.';

3b059b4f71a246acece230e49fc69985e03e1325
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 16:51:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Ea etelang makala nako le sesebelisoa oache kamahanyo.';
+$lang['date_app_name'] = 'Letsatsi le Time';
+$lang['date_app_tooltip'] = 'VoIP, ditshebeletso netefatso le ditshebediso tse ding hloka di-setting tsa nako e loketseng ea, kahoo ka kōpo etsa bonnete ba oache ea hao le zounu nako di nepahetse!';
+$lang['date_automatic_synchronize'] = 'jarolla ka ho iketsa bo nyalanya';
+$lang['date_date'] = 'letsatsi';
+$lang['date_synchronize'] = 'nyalanya';
+$lang['date_synchronize_now'] = 'nyalanya Joale';
+$lang['date_synchronize_wizard_tip'] = 'Le jarolla ka ho iketsa amahanya mabitso nolofalletsa, tsamaiso ea tla nyalanya oache ea eona nako le nako.';
+$lang['date_synchronized'] = 'tsamaisanang';
+$lang['date_synchronizing'] = 'amahanya';
+$lang['date_time'] = 'Time';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'Time seva ha o sebetse.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Time kamahanyo kemiso ha o sebetse.';
+$lang['date_time_zone'] = 'Time Zone';
+$lang['date_time_zone_is_invalid'] = 'Horaire ha o sebetse.';
+$lang['date_time_zone_wizard_help'] = 'ditiriso le ditshebeletso tse ngata batla nepahetse horaire tlhahisoleseding le e oache nepahetseng, kahoo ho ke ke bohlokwa hore e beha ka nepo.';

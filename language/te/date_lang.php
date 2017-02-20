53a333489ea420374d557559a89821690a1095b9
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 16:36:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'టైమ్ జోన్ మరియు గడియారం సమకాలీకరణ సాధనం.';
+$lang['date_app_name'] = 'తేదీ మరియు సమయం';
+$lang['date_app_tooltip'] = 'VoIP, ధృవీకరణ సేవలను మరియు ఇతర అప్లికేషన్లు, సరైన సమయం సెట్టింగ్లు అవసరం కాబట్టి మీ క్లాక్ మరియు టైమ్ జోన్లు సరిగ్గా ఉన్నాయని నిర్ధారించుకోండి దయచేసి!';
+$lang['date_automatic_synchronize'] = 'స్వయంచాలక సమకాలీకరించు';
+$lang['date_date'] = 'తేదీ';
+$lang['date_synchronize'] = 'సమకాలీకరించు';
+$lang['date_synchronize_now'] = 'ఇప్పుడు సమకాలీకరించు';
+$lang['date_synchronize_wizard_tip'] = 'ఆటోమేటిక్ సమకాలీకరించు ప్రారంభంతో, వ్యవస్థ ఎప్పటికప్పుడు దాని క్లాక్ సింక్రొనైజ్ అగును.';
+$lang['date_synchronized'] = 'సింక్రొనైజ్';
+$lang['date_synchronizing'] = 'సమకాలీకరణ';
+$lang['date_time'] = 'సమయం';
+$lang['date_time_server'] = 'టైమ్ సర్వర్';
+$lang['date_time_server_is_invalid'] = 'సమయం సర్వర్ చెల్లదు.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'సమయం సమకాలీకరణ షెడ్యూల్ చెల్లదు.';
+$lang['date_time_zone'] = 'సమయమండలం';
+$lang['date_time_zone_is_invalid'] = 'టైమ్ జోన్ చెల్లదు.';
+$lang['date_time_zone_wizard_help'] = 'అనేక అనువర్తనాలు మరియు సేవలు సరైన సమయం జోన్ సమాచారం మరియు ఖచ్చితమైన గడియారం అవసరం, కాబట్టి ఇది సరిగ్గా సెట్ అని ముఖ్యం.';

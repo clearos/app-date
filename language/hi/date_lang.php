dc5f172ae98a1282cbc91bb639f5c88e0ad9c72a
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 22:12:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'समय क्षेत्र और घड़ी तुल्यकालन उपकरण।';
+$lang['date_app_name'] = 'दिनांक और समय';
+$lang['date_app_tooltip'] = 'वीओआईपी, प्रमाणीकरण सेवाएं और अन्य अनुप्रयोगों, उचित समय सेटिंग्स की आवश्यकता होती है तो सुनिश्चित करें कि अपनी घड़ी और समय क्षेत्र सही हैं तो कृपया!';
+$lang['date_automatic_synchronize'] = 'स्वचालित सिंक्रनाइज़';
+$lang['date_date'] = 'तारीख';
+$lang['date_synchronize'] = 'सिंक्रनाइज़';
+$lang['date_synchronize_now'] = 'अब सिंक्रनाइज़';
+$lang['date_synchronize_wizard_tip'] = 'स्वत: सिंक्रनाइज़ सक्षम होने के साथ, सिस्टम को समय-समय पर अपनी घड़ी सिंक्रनाइज़ करेंगे।';
+$lang['date_synchronized'] = 'सिंक्रनाइज़';
+$lang['date_synchronizing'] = 'तुल्यकालन';
+$lang['date_time'] = 'पहर';
+$lang['date_time_server'] = 'समय सर्वर';
+$lang['date_time_server_is_invalid'] = 'समय सर्वर अमान्य है।';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'समय तुल्यकालन अनुसूची अमान्य है।';
+$lang['date_time_zone'] = 'समय क्षेत्र';
+$lang['date_time_zone_is_invalid'] = 'समय क्षेत्र अमान्य है।';
+$lang['date_time_zone_wizard_help'] = 'कई क्षुधा और सेवाओं के सही समय क्षेत्र की जानकारी और एक सटीक घड़ी की आवश्यकता होती है, इसलिए यह महत्वपूर्ण है कि यह ठीक से सेट है।';

a60f5e69fec4c9b31294f6a79d26340b2d87a49f
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 16:53:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'zone waqti iyo qalab .Wadashaqayntaas saacad.';
+$lang['date_app_name'] = 'Taariikhda iyo Waqtiga';
+$lang['date_app_tooltip'] = 'Ladhay, adeegyada sugida iyo codsiyada kale u baahan yihiin goobaha waqti munaasib ah, sidaas awgeed, fadlan hubi in aad saacad iyo zone waqti sax yihiin dhigi!';
+$lang['date_automatic_synchronize'] = 'automatic waafajinta';
+$lang['date_date'] = 'Taariikhda';
+$lang['date_synchronize'] = 'waafajinta';
+$lang['date_synchronize_now'] = 'waafajinta Hadda';
+$lang['date_synchronize_wizard_tip'] = 'Iyadoo waafajinta si toos ah awood, nidaamka waafajinta doonto saacad waqti ka waqti.';
+$lang['date_synchronized'] = 'beehsa';
+$lang['date_synchronizing'] = 'howlan';
+$lang['date_time'] = 'Time';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'server Time waa baadil.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'jadwalka Time .Wadashaqayntaas waa baadil.';
+$lang['date_time_zone'] = 'Time Zone';
+$lang['date_time_zone_is_invalid'] = 'zone Time waa baadil.';
+$lang['date_time_zone_wizard_help'] = 'apps iyo adeegyada badan ayaa u baahan macluumaad zone waqti sax ah oo saacad sax ah, sidaas darteed waxaa muhiim ah in si sax ah lagu wadaa.';

5db856547f1eae3d71da628ddd0b30b1aad8b661
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 21:15:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Laika joslu un pulksteni sinhronizācijas rīks.';
+$lang['date_app_name'] = 'Datums un laiks';
+$lang['date_app_tooltip'] = 'VoIP, autentifikācijas pakalpojumu un citas programmas prasa pareizu laika iestatījumus, tādēļ, lūdzu, pārliecinieties, ka jūsu pulkstenis un laika zona ir pareizi!';
+$lang['date_automatic_synchronize'] = 'Automātiskā sinhronizēšana';
+$lang['date_date'] = 'Datums';
+$lang['date_synchronize'] = 'sinhronizēt';
+$lang['date_synchronize_now'] = 'sinhronizēt tagad';
+$lang['date_synchronize_wizard_tip'] = 'Ar automātisko sinhronizēt aktivizēta, sistēma sinhronizēt savu pulksteni no laika uz laiku.';
+$lang['date_synchronized'] = 'Sinhronā';
+$lang['date_synchronizing'] = 'Sinhronizēšana';
+$lang['date_time'] = 'Laiks';
+$lang['date_time_server'] = 'izdabātājs';
+$lang['date_time_server_is_invalid'] = 'Laiks serveris ir nederīgs.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Laika sinhronizācija grafiks ir nederīgs.';
+$lang['date_time_zone'] = 'Laika zona';
+$lang['date_time_zone_is_invalid'] = 'Laika zona ir nederīgs.';
+$lang['date_time_zone_wizard_help'] = 'Daudz progr un pakalpojumiem prasa pareizu laika joslu informāciju un precīzu pulksteni, tāpēc ir svarīgi, ka tā ir iestatīts pareizi.';

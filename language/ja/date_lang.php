76188e5ee042d7851198dbf283628595dcff9518
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 21:48:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'タイムゾーンとクロック同期ツール。';
+$lang['date_app_name'] = '日時';
+$lang['date_app_tooltip'] = 'VoIPの、認証サービスやその他のアプリケーションの適切な時間設定を必要とするので、あなたの時計とタイムゾーンが正しいことを確認してください！';
+$lang['date_automatic_synchronize'] = '自動同期';
+$lang['date_date'] = '日付';
+$lang['date_synchronize'] = '同期します';
+$lang['date_synchronize_now'] = '今すぐ同期';
+$lang['date_synchronize_wizard_tip'] = '自動同期を有効にすると、システムは、時間から時間にクロックを同期させます。';
+$lang['date_synchronized'] = '同期されました';
+$lang['date_synchronizing'] = '同期化';
+$lang['date_time'] = '時間';
+$lang['date_time_server'] = 'タイムサーバ';
+$lang['date_time_server_is_invalid'] = 'タイムサーバーが無効です。';
+$lang['date_time_synchronization_schedule_is_invalid'] = '時刻同期スケジュールが無効です。';
+$lang['date_time_zone'] = 'タイムゾーン';
+$lang['date_time_zone_is_invalid'] = 'タイムゾーンは無効です。';
+$lang['date_time_zone_wizard_help'] = '多くのアプリケーションおよびサービスは、正しいタイムゾーン情報と正確なクロックを必要とするので、それが正しく設定されていることが重要です。';

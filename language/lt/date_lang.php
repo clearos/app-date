fefc310f91c3dbeddb2eee93cfd0cdf0dd5345b9
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 21:18:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Laiko juostos ir laikrodžio sinchronizacija įrankis.';
+$lang['date_app_name'] = 'Data ir laikas';
+$lang['date_app_tooltip'] = 'VoIP atpažinimo paslaugos ir kitos programos reikalauja tinkamo laiko nustatymus, todėl prašome įsitikinkite, kad jūsų laikas ir laiko zona yra teisinga!';
+$lang['date_automatic_synchronize'] = 'Automatinis Sinchronizacija';
+$lang['date_date'] = 'Data';
+$lang['date_synchronize'] = 'sinchronizuoti';
+$lang['date_synchronize_now'] = 'sinchronizuoti dabar';
+$lang['date_synchronize_wizard_tip'] = 'Su automatinio sinchronizuoti įjungta, sistema bus sinchronizuoti savo laikrodį, laikas nuo laiko.';
+$lang['date_synchronized'] = 'Sinchroninis';
+$lang['date_synchronizing'] = 'Sinchronizavimas';
+$lang['date_time'] = 'Laikas';
+$lang['date_time_server'] = 'laiko serveris';
+$lang['date_time_server_is_invalid'] = 'Laikas serveryje yra neteisinga.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Laiko sinchronizacija grafikas yra negaliojantis.';
+$lang['date_time_zone'] = 'Laiko zona';
+$lang['date_time_zone_is_invalid'] = 'Laiko juosta yra neteisinga.';
+$lang['date_time_zone_wizard_help'] = 'Daugelis programų ir paslaugų reikalauja teisingą laiko juostą ir tikslią laikrodį, todėl svarbu, kad jis yra nustatytas teisingai.';

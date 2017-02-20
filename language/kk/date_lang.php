20b4a0df30680957f21c9ce7fc030e69341d19bf
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 21:41:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'уақыт белдеуі және сағат үндестіру құралы.';
+$lang['date_app_name'] = 'Күні мен уақыты';
+$lang['date_app_tooltip'] = 'VoIP, аутентификация қызметтер мен басқа да қосымшалар тиісті уақыт параметрлерін қажет етеді, сондықтан сіздің сағаттар мен уақыт белдеуі дұрыс екендігіне көз жеткізіңіз!';
+$lang['date_automatic_synchronize'] = 'Автоматты синхрондау';
+$lang['date_date'] = 'дата';
+$lang['date_synchronize'] = 'синхрондау';
+$lang['date_synchronize_now'] = 'Енді синхрондау';
+$lang['date_synchronize_wizard_tip'] = 'автоматты синхрондау қосулы бірге, жүйе уақыт өте келе өз сағатын синхрондау болады.';
+$lang['date_synchronized'] = 'Синхронды';
+$lang['date_synchronizing'] = 'Қадамдастыру';
+$lang['date_time'] = 'уақыт';
+$lang['date_time_server'] = 'Уақыт Сервер';
+$lang['date_time_server_is_invalid'] = 'Уақыт сервері жарамсыз болып табылады.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Уақыт үндестіру кестесі жарамсыз болып табылады.';
+$lang['date_time_zone'] = 'Time Zone';
+$lang['date_time_zone_is_invalid'] = 'Уақыт белдеуі жарамсыз болып табылады.';
+$lang['date_time_zone_wizard_help'] = 'Көптеген бағдарламалар мен қызметтер дұрыс уақыт белдеуі туралы ақпаратты және дәл сағатты қажет етеді, сондықтан ол дұрыс орнатылғанын маңызды.';

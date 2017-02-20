fa7341e8acd039219b93691141d6da80a3d0ff45
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 17:55:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'समय क्षेत्र र घडी समिकरण उपकरण छ।';
+$lang['date_app_name'] = 'मिति र समय';
+$lang['date_app_tooltip'] = 'वीओआईपी, प्रमाणीकरण सेवा र अन्य आवेदन उचित समय सेटिङहरू आवश्यक, त्यसैले कृपया आफ्नो घडी र समय क्षेत्र सही हो निश्चित!';
+$lang['date_automatic_synchronize'] = 'स्वचालित सिंक्रोनाइज गर्नुहोस्';
+$lang['date_date'] = 'मिति';
+$lang['date_synchronize'] = 'सिंक्रोनाइज गर्नुहोस्';
+$lang['date_synchronize_now'] = 'अब सिंक्रोनाइज गर्नुहोस्';
+$lang['date_synchronize_wizard_tip'] = 'संग स्वचालित सिंक्रोनाइज गर्नुहोस् सक्षम, प्रणाली समय समयमा यसको घडी समक्रमण हुनेछ।';
+$lang['date_synchronized'] = 'लयात्मक';
+$lang['date_synchronizing'] = 'समक्रमण';
+$lang['date_time'] = 'समय';
+$lang['date_time_server'] = 'समय सर्भर';
+$lang['date_time_server_is_invalid'] = 'समय सर्भर अमान्य छ।';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'समय समिकरण तालिका अमान्य छ।';
+$lang['date_time_zone'] = 'समय क्षेत्र';
+$lang['date_time_zone_is_invalid'] = 'समय क्षेत्र अमान्य छ।';
+$lang['date_time_zone_wizard_help'] = 'धेरै अनुप्रयोगहरू र सेवाहरू सही समय क्षेत्र जानकारी र सही घडी आवश्यक, त्यसैले यो सही सेट गरिएको छ कि महत्त्वपूर्ण छ।';

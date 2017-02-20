ccf10eb1fec68abe05061014731ebffd3eb679f6
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 21:36:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'ಸಮಯ ವಲಯ ಮತ್ತು ಗಡಿಯಾರದ ಸಿಂಕ್ರೊನೈಸೇಶನ್ ಸಾಧನ.';
+$lang['date_app_name'] = 'ದಿನಾಂಕ ಮತ್ತು ಸಮಯ';
+$lang['date_app_tooltip'] = 'VoIP ಯು, ದೃಢೀಕರಣ ಸೇವೆಗಳನ್ನು ಮತ್ತು ಇತರ ಅಪ್ಲಿಕೇಶನ್ಗಳು, ಸರಿಯಾದ ಸಮಯ ಸೆಟ್ಟಿಂಗ್ಗಳನ್ನು ಅಗತ್ಯವಿರುತ್ತದೆ ನಿಮ್ಮ ಗಡಿಯಾರ ಮತ್ತು ಸಮಯ ವಲಯ ಸರಿಯಾಗಿವೆಯೇ ಎಂಬುದನ್ನು ಖಚಿತಪಡಿಸಿಕೊಳ್ಳಿ ದಯವಿಟ್ಟು!';
+$lang['date_automatic_synchronize'] = 'ಸ್ವಯಂಚಾಲಿತ ಸಿಂಕ್ರೊನೈಸ್';
+$lang['date_date'] = 'ದಿನಾಂಕ';
+$lang['date_synchronize'] = 'ಸಿಂಕ್ರೊನೈಸ್';
+$lang['date_synchronize_now'] = 'ಈಗ ಸಿಂಕ್ರೊನೈಸ್';
+$lang['date_synchronize_wizard_tip'] = 'ಸ್ವಯಂಚಾಲಿತ ಸಿಂಕ್ರೊನೈಸ್ ಸಕ್ರಿಯತೆಯೊಂದಿಗೆ, ವ್ಯವಸ್ಥೆ ಕಾಲಕಾಲಕ್ಕೆ ತನ್ನ ಗಡಿಯಾರ ಸಿಂಕ್ರೊನೈಸ್ ಮಾಡುತ್ತದೆ.';
+$lang['date_synchronized'] = 'ಸಿಂಕ್ರೊನೈಸ್';
+$lang['date_synchronizing'] = 'ಸಿಂಕ್ರೊನೈಸ್';
+$lang['date_time'] = 'ಟೈಮ್';
+$lang['date_time_server'] = 'ಸಮಯ ಪರಿಚಾರಕ';
+$lang['date_time_server_is_invalid'] = 'ಸಮಯ ಪರಿಚಾರಕ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'ಸಮಯ ಸಿಂಕ್ರೊನೈಸೇಶನ್ ವೇಳಾಪಟ್ಟಿ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['date_time_zone'] = 'ಸಮಯ ವಲಯ';
+$lang['date_time_zone_is_invalid'] = 'ಕಾಲವಲಯ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['date_time_zone_wizard_help'] = 'ಅನೇಕ ಅಪ್ಲಿಕೇಶನ್ಗಳು ಮತ್ತು ಸೇವೆಗಳನ್ನು ಸರಿಯಾದ ಸಮಯ ವಲಯ ಮಾಹಿತಿ ಮತ್ತು ನಿಖರವಾದ ಗಡಿಯಾರ ಅಗತ್ಯವಿದೆ, ಆದ್ದರಿಂದ ಸರಿಯಾಗಿ ನಮೂದಿಸಲಾಗಿದೆ ಎಂದು ಮುಖ್ಯ.';

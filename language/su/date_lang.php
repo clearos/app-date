d568fb2b4c1b599a64f32d5d78d65d8e0c0c7670
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 16:46:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Zona waktu jeung alat sinkronisasi jam.';
+$lang['date_app_name'] = 'Tanggal jeung Zona';
+$lang['date_app_tooltip'] = 'VoIP, jasa auténtikasi jeung aplikasi nu sejen merlukeun setelan waktu nu ditangtoskeun, jadi mangga pastikeun jam jeung zona waktu nu bener!';
+$lang['date_automatic_synchronize'] = 'nyingkronkeun otomatis';
+$lang['date_date'] = 'Kaping';
+$lang['date_synchronize'] = 'nyingkronkeun';
+$lang['date_synchronize_now'] = 'nyingkronkeun Ayeuna';
+$lang['date_synchronize_wizard_tip'] = 'Jeung singkronkeun otomatis diaktipkeun, sistim bakal nyinkronkeun jam anak ti jaman ka jaman.';
+$lang['date_synchronized'] = 'nyingkronkeun';
+$lang['date_synchronizing'] = 'Nyingkronkeun';
+$lang['date_time'] = 'waktu';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'server waktu anu sah.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'jadwal sinkronisasi waktu anu sah.';
+$lang['date_time_zone'] = 'Jona Wayah';
+$lang['date_time_zone_is_invalid'] = 'zona waktu anu sah.';
+$lang['date_time_zone_wizard_help'] = 'Loba aktip jeung jasa merlukeun informasi zona waktu bener jeung jam akurat, ku kituna penting nu diatur bener.';

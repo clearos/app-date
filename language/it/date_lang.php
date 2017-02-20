214c95fb928ed377f9e12c86fc96f7b524975c0d
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 21:52:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/it/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Il fuso orario e strumento di sincronizzazione dell'orologio.';
+$lang['date_app_name'] = 'Data e ora';
+$lang['date_app_tooltip'] = 'VoIP, servizi di autenticazione e altre applicazioni richiedono impostazioni momento giusto, in modo da assicurarsi il vostro orologio e il fuso orario siano corretti!';
+$lang['date_automatic_synchronize'] = 'Sincronizza automatica';
+$lang['date_date'] = 'Data';
+$lang['date_synchronize'] = 'Sincronizzare';
+$lang['date_synchronize_now'] = 'Sincronizza';
+$lang['date_synchronize_wizard_tip'] = 'Con sincronizzazione automatica attivata, il sistema sincronizzare il suo orologio di volta in volta.';
+$lang['date_synchronized'] = 'sincronizzato';
+$lang['date_synchronizing'] = 'Sincronizzazione';
+$lang['date_time'] = 'Tempo';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'Time server non è valido.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'pianificazione di sincronizzazione tempo è valido.';
+$lang['date_time_zone'] = 'Fuso orario';
+$lang['date_time_zone_is_invalid'] = 'Fuso orario non è valido.';
+$lang['date_time_zone_wizard_help'] = 'Molte applicazioni e servizi richiedono una corretta informazione fuso orario e un orologio preciso, quindi è importante che sia impostata correttamente.';

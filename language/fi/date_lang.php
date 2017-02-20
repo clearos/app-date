cd4af110dd26098a0867ad26ed52501cc3233f5d
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 22:51:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Aikavyöhykkeen ja kellon synkronointi työkalu.';
+$lang['date_app_name'] = 'Päivämäärä ja aika';
+$lang['date_app_tooltip'] = 'VoIP, todennus ja muut sovellukset vaativat oikea aika-asetukset, joten varmista kellon ja aikavyöhykkeen ovat oikein!';
+$lang['date_automatic_synchronize'] = 'Automaattinen Synkronoi';
+$lang['date_date'] = 'Treffi';
+$lang['date_synchronize'] = 'Synkronoida';
+$lang['date_synchronize_now'] = 'synkronoida Nyt';
+$lang['date_synchronize_wizard_tip'] = 'Automaattisella synkronoida käytössä, järjestelmä synkronoi kellonsa ajoittain.';
+$lang['date_synchronized'] = 'Synkronoitu';
+$lang['date_synchronizing'] = 'synkronointi';
+$lang['date_time'] = 'Aika';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'Aika-palvelin on virheellinen.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Ajan synkronointi aikataulu on virheellinen.';
+$lang['date_time_zone'] = 'Aikavyöhyke';
+$lang['date_time_zone_is_invalid'] = 'Aikavyöhyke on virheellinen.';
+$lang['date_time_zone_wizard_help'] = 'Monet sovellukset ja palvelut vaativat oikean aikavyöhykkeen tietoja ja tarkkaa kelloa, joten on tärkeää, että se on oikein.';

126188cbccfaad283ea67e6c9be802e8aeb85573
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 18:48:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'цаг хугацаа бүс, цаг синхрончлол хэрэгсэл юм.';
+$lang['date_app_name'] = 'Огноо ба цаг хугацаа';
+$lang['date_app_tooltip'] = 'VoIP, нэвтрэлт танилт үйлчилгээ болон бусад програмууд, зохих цаг хугацаа тохиргоо шаардлагатай тул таны цаг, цагийн бүс зөв эсэхээ шалгана уу!';
+$lang['date_automatic_synchronize'] = 'автомат хамгийн сүүлийн хэлбэрт аваачиж';
+$lang['date_date'] = 'огноо';
+$lang['date_synchronize'] = 'синхрончлох';
+$lang['date_synchronize_now'] = 'Одоо хамгийн сүүлийн хэлбэрт аваачиж';
+$lang['date_synchronize_wizard_tip'] = 'автомат синхрончлолын идэвхжсэн нь, систем үе үе түүний цагийг хамгийн сүүлийн хэлбэрт аваачиж болно.';
+$lang['date_synchronized'] = 'синхрон';
+$lang['date_synchronizing'] = 'хамгийн сүүлийн хэлбэрт аваачих';
+$lang['date_time'] = 'цаг';
+$lang['date_time_server'] = 'цаг Server';
+$lang['date_time_server_is_invalid'] = 'Цаг серверийн хүчин төгөлдөр бус байна.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Цаг тааруулах хуваарь хүчин төгөлдөр бус байна.';
+$lang['date_time_zone'] = 'Цагийн бүс';
+$lang['date_time_zone_is_invalid'] = 'Цагийн бүс хүчин төгөлдөр бус байна.';
+$lang['date_time_zone_wizard_help'] = 'Олон апп-ууд, үйлчилгээ зөв цагийн бүс мэдээлэл, үнэн зөв цагийг шаарддаг, тиймээс энэ нь зөв тогтоох нь чухал юм.';

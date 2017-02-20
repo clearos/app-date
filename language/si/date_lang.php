26077159bc635f9108c19085484a0c411659e004
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 17:00:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'වේලා කලාපය හා ඔරලෝසුව සමමුහුර්ත මෙවලමකි.';
+$lang['date_app_name'] = 'දිනය සහ වේලාව';
+$lang['date_app_tooltip'] = 'ගොඩක් සතුටුයි, සත්යාපනය සේවාවන් සහ වෙනත් අයදුම්පත් නිසි වේලාව සැකසීම අවශ්ය, ඒ නිසා කරුණාකර ඔබේ ඔරලෝසුව සහ කාල කලාප නිවැරදි දැයි සැකහැර!';
+$lang['date_automatic_synchronize'] = 'ස්වයංක්රීය සමකාලී';
+$lang['date_date'] = 'දිනය';
+$lang['date_synchronize'] = 'සමමුහුර්ත කරන්න';
+$lang['date_synchronize_now'] = 'දැන් සමමුහුර්ත කරන්න';
+$lang['date_synchronize_wizard_tip'] = 'ස්වයංක්රීය සමකාලී සක්රීය, පද්ධතිය වරින් වර සිය ඔරලෝසුව අතළොස්සට.';
+$lang['date_synchronized'] = 'සමමුහුර්ත';
+$lang['date_synchronizing'] = 'සමමුහුර්ත';
+$lang['date_time'] = 'කාලය';
+$lang['date_time_server'] = 'කාලය සේවාදායකය';
+$lang['date_time_server_is_invalid'] = 'කාල සේවාදායකය වලංගු නැත.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'කාලය සමමුහුර්ත කාලසටහන වලංගු නැත.';
+$lang['date_time_zone'] = 'වේලා කලාපය';
+$lang['date_time_zone_is_invalid'] = 'වේලා කලාපය වලංගු නැත.';
+$lang['date_time_zone_wizard_help'] = 'බොහෝ යෙදුම් හා සේවා නිවැරදි කාල කලාපය තොරතුරු හා නිවැරදි ඔරලෝසුව අවශ්ය, ඒ නිසා එය නිවැරදිව සකස් කර ගැනීම වැදගත් වෙනවා.';

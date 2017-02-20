3cb39d9981ede1a15a582c06cdfdc4de7b6b5411
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 22:01:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Oge mpaghara na elekere mmekọrịta ngwá ọrụ.';
+$lang['date_app_name'] = 'Ụbọchị na oge';
+$lang['date_app_tooltip'] = 'VoIP, Nyocha ọrụ ndị ọzọ ngwa-achọ kwesịrị ekwesị ntọala, ya mere, biko jide n&#39;aka gị elekere na oge mpaghara ndị ziri ezi!';
+$lang['date_automatic_synchronize'] = 'akpaka mekọrịta';
+$lang['date_date'] = 'Ụbọchị';
+$lang['date_synchronize'] = 'mekọrịta';
+$lang['date_synchronize_now'] = 'mekọrịta Ugbu a';
+$lang['date_synchronize_wizard_tip'] = 'Na akpaka mekọrịta nyeere, usoro ga-mekọrịta ya elekere site n&#39;oge ruo n&#39;oge.';
+$lang['date_synchronized'] = 'synchronized';
+$lang['date_synchronizing'] = 'Ịme mmekọrịta';
+$lang['date_time'] = 'Time';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'Time server bụ ghara ịdị irè.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Time mmekọrịta oge bụ ghara ịdị irè.';
+$lang['date_time_zone'] = 'Time Mpaghara';
+$lang['date_time_zone_is_invalid'] = 'Oge mpaghara bụ ghara ịdị irè.';
+$lang['date_time_zone_wizard_help'] = 'Ọtụtụ ngwa ọdịnala na ọrụ na-achọ ezi oge mpaghara ọmụma na ezi ihe elekere, n&#39;ihi ya, ọ dị mkpa na ọ ga n&#39;ụzọ ziri ezi.';

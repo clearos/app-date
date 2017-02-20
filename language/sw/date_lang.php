ece2fbc5bcdef7c7e356be39c3acac4975a4d4c4
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 16:43:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'wakati eneo na chombo saa maingiliano.';
+$lang['date_app_name'] = 'Tarehe na Muda';
+$lang['date_app_tooltip'] = 'VoIP, Huduma za kujitambulisha na maombi mengine yanahitaji mazingira sahihi wakati, hivyo tafadhali kuhakikisha saa yako na wakati eneo ni sahihi!';
+$lang['date_automatic_synchronize'] = 'moja kwa moja Synchronize';
+$lang['date_date'] = 'Tarehe';
+$lang['date_synchronize'] = 'synchronize';
+$lang['date_synchronize_now'] = 'synchronize Sasa';
+$lang['date_synchronize_wizard_tip'] = 'Na moja kwa moja synchronize kuwezeshwa, mfumo wa synchronize saa yake ya mara kwa mara.';
+$lang['date_synchronized'] = 'Mfumo huo';
+$lang['date_synchronizing'] = 'synchronizing';
+$lang['date_time'] = 'muda';
+$lang['date_time_server'] = 'muda Server';
+$lang['date_time_server_is_invalid'] = 'Muda server ni batili.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Wakati maingiliano ratiba ni batili.';
+$lang['date_time_zone'] = 'Eneo La muda';
+$lang['date_time_zone_is_invalid'] = 'Wakati eneo ni batili.';
+$lang['date_time_zone_wizard_help'] = 'programu na huduma nyingi zinahitaji sahihi wakati eneo habari na saa sahihi, hivyo ni muhimu kwamba ni kuweka kwa usahihi.';

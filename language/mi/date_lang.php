26459935538410a32a221c02236d403fed7d5bd0
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 21:09:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Te rohe wā, me te karaka tukutahitanga taputapu.';
+$lang['date_app_name'] = 'Rā me te Wā';
+$lang['date_app_tooltip'] = 'VoIP, ratonga motuhēhēnga me ētahi atu taupānga rapu tautuhinga wa e tika, kia pai kia koutou karaka me te rohe wā e tika!';
+$lang['date_automatic_synchronize'] = 'Tukutahi Aunoa';
+$lang['date_date'] = 'rā';
+$lang['date_synchronize'] = 'tukutahi';
+$lang['date_synchronize_now'] = 'tukutahi Na';
+$lang['date_synchronize_wizard_tip'] = 'Ki te tukutahi aunoa whakahohea, ka tukutahi i te pūnaha tona karaka i te wa wa ki.';
+$lang['date_synchronized'] = 'tukutahi';
+$lang['date_synchronizing'] = 'tukutahi';
+$lang['date_time'] = 'wā';
+$lang['date_time_server'] = 'Tūmau wā';
+$lang['date_time_server_is_invalid'] = 'He muhu Time tūmau.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'He muhu hōtaka tukutahitanga Time.';
+$lang['date_time_zone'] = 'Rohe wā';
+$lang['date_time_zone_is_invalid'] = 'He muhu rohe Time.';
+$lang['date_time_zone_wizard_help'] = 'He maha taupānga me ngā ratonga rapu tika mōhiohio rohe wā me te karaka tika, na he mea nui e te whakaturia te reira tika.';

766d19e203dd4eaf0adf0d347a03d1589f4d1c9d
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 22:28:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'સમય ઝોન અને ઘડિયાળ સુમેળ સાધન.';
+$lang['date_app_name'] = 'તારીખ અને સમય';
+$lang['date_app_tooltip'] = 'વીઓઆઈપી, સત્તાધિકરણ સેવાઓ અને અન્ય કાર્યક્રમો, યોગ્ય સમય ને જરૂર છે જેથી ખાતરી કરો કે તમારી ઘડિયાળ અને સમય ઝોન યોગ્ય છે તેની ખાતરી કરો!';
+$lang['date_automatic_synchronize'] = 'આપોઆપ સુમેળ';
+$lang['date_date'] = 'તારીખ';
+$lang['date_synchronize'] = 'સમન્વય';
+$lang['date_synchronize_now'] = 'હવે સમન્વય';
+$lang['date_synchronize_wizard_tip'] = 'આપોઆપ સુમેળ સક્ષમ કરતાની સાથે, સિસ્ટમ સમય સમય પર તેની ઘડિયાળ સુમેળ કરશે.';
+$lang['date_synchronized'] = 'સુમેળ';
+$lang['date_synchronizing'] = 'સુમેળ';
+$lang['date_time'] = 'સમય';
+$lang['date_time_server'] = 'જ્યારે સર્વર';
+$lang['date_time_server_is_invalid'] = 'જ્યારે સર્વર અમાન્ય છે.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'સમય સુમેળ શેડ્યૂલ અમાન્ય છે.';
+$lang['date_time_zone'] = 'સમય ઝોન';
+$lang['date_time_zone_is_invalid'] = 'સમય ઝોન અમાન્ય છે.';
+$lang['date_time_zone_wizard_help'] = 'ઘણા એપ્લિકેશન્સ અને સેવાઓ યોગ્ય ટાઈમ ઝોનમાં માહિતી અને ચોક્કસ ઘડિયાળ જરૂર છે, તેથી તે મહત્વનું છે તે યોગ્ય રીતે સુયોજિત છે કે.';

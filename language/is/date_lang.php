7a0538d7cf648e90eb2579bcba4413dc5196ac89
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 21:57:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Tímabelti og klukka samstillingu tól.';
+$lang['date_app_name'] = 'Dagsetning og tími';
+$lang['date_app_tooltip'] = 'VoIP, sannvottun þjónustu og önnur forrit krefjast réttar tíma, svo vinsamlegast vertu viss um að klukkan í og ​​tímabelti eru rétt!';
+$lang['date_automatic_synchronize'] = 'Sjálfvirk Samstilla';
+$lang['date_date'] = 'Dagsetning';
+$lang['date_synchronize'] = 'Samstilling';
+$lang['date_synchronize_now'] = 'Samstilling Nú';
+$lang['date_synchronize_wizard_tip'] = 'Með sjálfvirkri Samstilling virkt, mun kerfið samstilla klukkuna sína frá einum tíma til annars.';
+$lang['date_synchronized'] = 'samstillt';
+$lang['date_synchronizing'] = 'Samstilling';
+$lang['date_time'] = 'tími';
+$lang['date_time_server'] = 'tími Server';
+$lang['date_time_server_is_invalid'] = 'Tími framreiðslumaður er ógilt.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Tímastilling áætlun er ógilt.';
+$lang['date_time_zone'] = 'Tímabelti';
+$lang['date_time_zone_is_invalid'] = 'Tímabelti er ógilt.';
+$lang['date_time_zone_wizard_help'] = 'Margir forrit og þjónustu þurfa réttar tímabelti og nákvæma klukku, þannig að það er mikilvægt að það sé rétt stillt.';

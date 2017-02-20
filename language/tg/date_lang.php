4a9c1ecf75700812e6b125dafc128b4d48123e9e
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 16:33:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Дар минтақаи вақт ва асбоби синхронизатсияи соати.';
+$lang['date_app_name'] = 'Сана ва вақт';
+$lang['date_app_tooltip'] = 'VoIP, хадамоти аутентификатсия ва барномаҳои дигар талаб танзимоти вақт, то лутфан тафтиш кунед, шабонарӯзӣ ва минтақаи вақти дуруст аст!';
+$lang['date_automatic_synchronize'] = 'ҳамоҳангсозии худкори';
+$lang['date_date'] = 'таърих';
+$lang['date_synchronize'] = 'ҳамоҳанг';
+$lang['date_synchronize_now'] = 'Акнун ҳамоҳанг';
+$lang['date_synchronize_wizard_tip'] = 'Бо ҳамоҳангсозии таври худкор, система шабонарӯзӣ он вақт аз вақт ҳамоҳангсозии.';
+$lang['date_synchronized'] = 'ҳамоҳанг';
+$lang['date_synchronizing'] = 'синхронизатсия';
+$lang['date_time'] = 'Time';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'сервер Time нодуруст аст.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'ҷадвали Ҳамоҳангсозӣ Time нодуруст аст.';
+$lang['date_time_zone'] = 'Минтақаи Time';
+$lang['date_time_zone_is_invalid'] = 'озоди Time нодуруст аст.';
+$lang['date_time_zone_wizard_help'] = 'Бисёре аз барномаҳо ва хизматрасониҳои талаб маълумот минтақаи вақти дуруст ва соати дақиқи, то он муҳим аст, ки он дуруст муқаррар карда мешавад.';

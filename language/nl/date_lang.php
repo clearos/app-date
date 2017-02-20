a9cd137e45d455dc3a15d6205e16297abb9abe88
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 17:52:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'De tijdzone en synchronisatie tool.';
+$lang['date_app_name'] = 'Datum en tijd';
+$lang['date_app_tooltip'] = 'VoIP, authenticatie en andere toepassingen vereisen de juiste tijd instellingen, dus zorg ervoor dat uw klok en tijdzone correct zijn!';
+$lang['date_automatic_synchronize'] = 'automatisch synchroniseren';
+$lang['date_date'] = 'Datum';
+$lang['date_synchronize'] = 'Synchroniseren';
+$lang['date_synchronize_now'] = 'Synchroniseer Nu';
+$lang['date_synchronize_wizard_tip'] = 'Met automatische synchroniseren is ingeschakeld, zal het systeem de klok van tijd tot tijd te synchroniseren.';
+$lang['date_synchronized'] = 'gesynchroniseerd';
+$lang['date_synchronizing'] = 'Synchroniseren';
+$lang['date_time'] = 'Tijd';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'Time server is ongeldig.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Tijdsynchronisatie schema is ongeldig.';
+$lang['date_time_zone'] = 'Tijdzone';
+$lang['date_time_zone_is_invalid'] = 'Tijdzone is ongeldig.';
+$lang['date_time_zone_wizard_help'] = 'Veel apps en diensten vereisen juiste tijdzone informatie en een nauwkeurige klok, dus het is belangrijk dat het correct is ingesteld.';

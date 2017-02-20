496861ef08601da603f1f28af6b86a385c771dd0
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 21:07:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Зоната време и алатка часовник синхронизација.';
+$lang['date_app_name'] = 'Датум и време';
+$lang['date_app_tooltip'] = 'VoIP, автентикација услуги и други апликации бараат соодветна форматот на времето, па ве молиме бидете сигурни дека вашиот часовник и временска зона се точни!';
+$lang['date_automatic_synchronize'] = 'автоматско Синхронизирај';
+$lang['date_date'] = 'датум';
+$lang['date_synchronize'] = 'синхронизирате';
+$lang['date_synchronize_now'] = 'Синхронизирај сега';
+$lang['date_synchronize_wizard_tip'] = 'Со автоматски ги синхронизирате е овозможено, системот ќе ги синхронизира часовникот од време на време.';
+$lang['date_synchronized'] = 'синхронизирани';
+$lang['date_synchronizing'] = 'Синхронизирање';
+$lang['date_time'] = 'време';
+$lang['date_time_server'] = 'време на серверот';
+$lang['date_time_server_is_invalid'] = 'Време на серверот не е валиден.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'распоред синхронизација на време не е валиден.';
+$lang['date_time_zone'] = 'Временска зона';
+$lang['date_time_zone_is_invalid'] = 'Временска зона не е валиден.';
+$lang['date_time_zone_wizard_help'] = 'Многу апликации и услуги бараат точни информации временска зона и прецизен часовник, па затоа е важно дека тоа е правилно поставена.';

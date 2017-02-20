abad485c4a631acdb2531d7160ab7e333aa42cb6
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 17:08:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Fusul orar și instrumentul de sincronizare a ceasului.';
+$lang['date_app_name'] = 'Data si ora';
+$lang['date_app_tooltip'] = 'VoIP, servicii de autentificare și alte aplicații necesită setări de timp corespunzătoare, astfel încât vă rugăm să vă asigurați că ora și fusul orar sunt corecte!';
+$lang['date_automatic_synchronize'] = 'automată Sincronizare';
+$lang['date_date'] = 'Data';
+$lang['date_synchronize'] = 'Sincroniza';
+$lang['date_synchronize_now'] = 'Sincronizare acum';
+$lang['date_synchronize_wizard_tip'] = 'Cu sincroniza automat activat, sistemul va sincroniza ceasul din când în când.';
+$lang['date_synchronized'] = 'sincronizat';
+$lang['date_synchronizing'] = 'Sincronizarea';
+$lang['date_time'] = 'Timp';
+$lang['date_time_server'] = 'Server time';
+$lang['date_time_server_is_invalid'] = 'server de timp nu este valid.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'program de sincronizare de timp nu este validă.';
+$lang['date_time_zone'] = 'Fus orar';
+$lang['date_time_zone_is_invalid'] = 'Fus orar este nevalid.';
+$lang['date_time_zone_wizard_help'] = 'Multe aplicații și servicii necesită informații corecte de fus orar și un ceas precis, de aceea este important ca acesta este setat corect.';

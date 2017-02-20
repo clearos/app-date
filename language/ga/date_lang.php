94f3fb3a3cfd246215b6c694c4c882f71a05fb95
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 22:03:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'An crios ama agus uirlis sioncrónaithe, clog.';
+$lang['date_app_name'] = 'Dáta agus Am';
+$lang['date_app_tooltip'] = 'VoIP, a cheangal ar na seirbhísí fíordheimhnithe agus iarratais eile suímh am cuí, le do thoil déan cinnte go bhfuil do clog agus crios ama ceart!';
+$lang['date_automatic_synchronize'] = 'Uathoibríoch Sioncrónaigh';
+$lang['date_date'] = 'dáta';
+$lang['date_synchronize'] = 'Sioncrónaigh';
+$lang['date_synchronize_now'] = 'Sioncrónaigh Anois';
+$lang['date_synchronize_wizard_tip'] = 'Le sioncrónú uathoibríoch chumas, beidh an córas sioncrónú a chloig ó am go ham.';
+$lang['date_synchronized'] = 'synchronized';
+$lang['date_synchronizing'] = 'synchronizing';
+$lang['date_time'] = 'Am';
+$lang['date_time_server'] = 'Am Freastalaí';
+$lang['date_time_server_is_invalid'] = 'Is freastalaí Am neamhbhailí.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Is sceideal ama sioncrónaithe neamhbhailí.';
+$lang['date_time_zone'] = 'Crios Ama';
+$lang['date_time_zone_is_invalid'] = 'Is crios ama neamhbhailí.';
+$lang['date_time_zone_wizard_help'] = 'A cheangal go leor apps agus seirbhísí eolais crios ama cruinn agus clog cruinn, mar sin tá sé tábhachtach go bhfuil sé socraithe i gceart.';

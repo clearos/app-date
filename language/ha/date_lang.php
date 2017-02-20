680fa9c203fd51b2e0314be119270c604c99f014
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 22:19:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'A lokacin da zone da agogo aiki tare da kayan aiki.';
+$lang['date_app_name'] = 'Kwanan wata da lokaci';
+$lang['date_app_tooltip'] = 'VoIP, Tantance kalmar sirri ayyuka da kuma sauran aikace-aikace na bukatar kari saituna, don haka don Allah ka tabbata ka agogo da kuma lokacin zone ne daidai!';
+$lang['date_automatic_synchronize'] = 'atomatik aiki tare';
+$lang['date_date'] = 'kwanan';
+$lang['date_synchronize'] = 'Haɗa aiki tare da';
+$lang['date_synchronize_now'] = 'Haɗa aiki tare da Now';
+$lang['date_synchronize_wizard_tip'] = 'Tare da m aiki tare sa, da tsarin za su aiki tare da agogo daga lokaci zuwa lokaci.';
+$lang['date_synchronized'] = 'aiki';
+$lang['date_synchronizing'] = 'Aiki tare';
+$lang['date_time'] = 'Time';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'Time uwar garken ba daidai ba ne.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Time aiki tare jadawalin ba daidai ba ne.';
+$lang['date_time_zone'] = 'Time Zone';
+$lang['date_time_zone_is_invalid'] = 'Time zone ba daidai ba ne.';
+$lang['date_time_zone_wizard_help'] = 'Mutane da yawa apps da kuma ayyuka na bukatar daidai lokaci zone bayanai da kuma wani m agogo, don haka yana da muhimmanci cewa an saita daidai.';

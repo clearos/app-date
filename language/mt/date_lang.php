73b024a372e6bbae19906039035f8fcb12ea3de8
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 17:59:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Iż-żona ħin u għodda sinkronizzazzjoni arloġġ.';
+$lang['date_app_name'] = 'Data u Ħin';
+$lang['date_app_tooltip'] = 'VoIP, servizzi ta &#39;awtentikazzjoni u applikazzjonijiet oħra jeħtieġu settings tal-ħin xierqa, għalhekk jekk jogħġbok kun żgur arloġġ tiegħek u żona ta&#39; ħin huma korretti!';
+$lang['date_automatic_synchronize'] = 'awtomatika Synchronize';
+$lang['date_date'] = 'data';
+$lang['date_synchronize'] = 'synchronize';
+$lang['date_synchronize_now'] = 'synchronize Issa';
+$lang['date_synchronize_wizard_tip'] = 'Bil jissinkronizzaw awtomatiku attivat, is-sistema se jissinkronizzaw arloġġ tiegħu minn żmien għal żmien.';
+$lang['date_synchronized'] = 'sinkronizzata';
+$lang['date_synchronizing'] = 'sinkronizzanti';
+$lang['date_time'] = 'ħin';
+$lang['date_time_server'] = 'ħin Server';
+$lang['date_time_server_is_invalid'] = 'Ħin server huwa invalidu.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'iskeda sinkronizzazzjoni Ħin huwa invalidu.';
+$lang['date_time_zone'] = 'Żona tal-ħin';
+$lang['date_time_zone_is_invalid'] = 'żona ta &#39;żmien huwa invalidu.';
+$lang['date_time_zone_wizard_help'] = 'Ħafna apps u s-servizzi jirrikjedu informazzjoni korretta żona ta &#39;ħin u arloġġ preċiż, u għalhekk huwa importanti li din tiġi stabbilita b&#39;mod korrett.';

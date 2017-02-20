9bfd63c2d4c2928ee99a98f8a8332c5443e80001
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 21:24:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Tempus zona horologium synchronization et instrumentum.';
+$lang['date_app_name'] = 'Dies et tempus';
+$lang['date_app_tooltip'] = 'VoIP, authenticas propria officia et usus simul Occasus, planto certus ut velit tempus zona horologium et vera sunt!';
+$lang['date_automatic_synchronize'] = 'Automatic Synchronize';
+$lang['date_date'] = 'Date';
+$lang['date_synchronize'] = 'synchronize';
+$lang['date_synchronize_now'] = 'Sed synchronize';
+$lang['date_synchronize_wizard_tip'] = 'Automatic synchronize enabled, quod ratio dolor synchronize suum de tempore usque ad tempus.';
+$lang['date_synchronized'] = 'synchronized';
+$lang['date_synchronizing'] = 'synchronum';
+$lang['date_time'] = 'Tempus';
+$lang['date_time_server'] = 'Tempus Server';
+$lang['date_time_server_is_invalid'] = 'Servo tempus nullum est.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Synchronization schedule tempus nullum est.';
+$lang['date_time_zone'] = 'Zona temporis';
+$lang['date_time_zone_is_invalid'] = 'Temporalis est invalidum.';
+$lang['date_time_zone_wizard_help'] = 'Multis apps, et servitia requirit rectam tempus zona horologium notitia, et diligenter, sic est maximus ut bene sit.';

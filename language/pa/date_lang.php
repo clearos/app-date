b834e46ef149089cc282d957f25bace9d4c5faa0
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 17:27:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'ਟਾਈਮ ਜ਼ੋਨ ਅਤੇ ਕਲਾਕ ਸਮਕਾਲਤਾ ਸੰਦ ਹੈ.';
+$lang['date_app_name'] = 'ਮਿਤੀ ਅਤੇ ਟਾਈਮ';
+$lang['date_app_tooltip'] = 'VoIP, ਪ੍ਰਮਾਣਿਕਤਾ ਸੇਵਾ ਅਤੇ ਹੋਰ ਕਾਰਜ, ਸਹੀ ਟਾਈਮ ਸੈਟਿੰਗ ਦੀ ਲੋੜ ਹੈ, ਇਸ ਲਈ ਯਕੀਨੀ ਬਣਾਓ ਕਿ ਤੁਹਾਡੇ ਘੜੀ ਹੈ ਅਤੇ ਵਾਰ ਜ਼ੋਨ ਠੀਕ ਹਨ, ਕਿਰਪਾ ਕਰਕੇ!';
+$lang['date_automatic_synchronize'] = 'ਆਟੋਮੈਟਿਕ ਸਮਕਾਲੀ';
+$lang['date_date'] = 'ਮਿਤੀ';
+$lang['date_synchronize'] = 'ਸਮਕਾਲੀ';
+$lang['date_synchronize_now'] = 'ਹੁਣ ਸਮਕਾਲੀ';
+$lang['date_synchronize_wizard_tip'] = 'ਆਟੋਮੈਟਿਕ ਸਮਕਾਲੀ ਨੂੰ ਯੋਗ ਨਾਲ, ਸਿਸਟਮ ਵਾਰ ਤੱਕ ਵਾਰ ਕਰਨ ਲਈ ਇਸ ਦੇ ਘੜੀ ਸਮਕਾਲੀ ਕਰੇਗਾ.';
+$lang['date_synchronized'] = 'ਸਮਕਾਲੀ';
+$lang['date_synchronizing'] = 'ਸਮਕਾਲੀ';
+$lang['date_time'] = 'ਟਾਈਮ';
+$lang['date_time_server'] = 'ਟਾਈਮ ਸਰਵਰ';
+$lang['date_time_server_is_invalid'] = 'ਟਾਈਮ ਸਰਵਰ ਗਲਤ ਹੈ.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'ਟਾਈਮ ਸਮਕਾਲੀ ਤਹਿ ਗਲਤ ਹੈ.';
+$lang['date_time_zone'] = 'ਸਮਾਂ ਖੇਤਰ';
+$lang['date_time_zone_is_invalid'] = 'ਟਾਈਮ ਜ਼ੋਨ ਗਲਤ ਹੈ.';
+$lang['date_time_zone_wizard_help'] = 'ਕਈ ਐਪਸ ਅਤੇ ਸੇਵਾ ਨੂੰ ਸਹੀ ਟਾਈਮ ਜ਼ੋਨ ਜਾਣਕਾਰੀ ਅਤੇ ਸਹੀ ਘੜੀ ਦੀ ਲੋੜ ਹੈ, ਇਸ ਲਈ ਇਹ ਜ਼ਰੂਰੀ ਹੈ ਕਿ ਇਹ ਠੀਕ ਹੈ ਕਿ.';

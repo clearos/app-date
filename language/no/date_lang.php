310b364e9b3401bff36d31b66461c771500e721f
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 17:34:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Tidssone og klokkesynkronisering verktøyet.';
+$lang['date_app_name'] = 'Dato og tid';
+$lang['date_app_tooltip'] = 'VoIP, autentiseringstjenester og andre programmer krever riktige tidsinnstillinger, så sørg for at din klokke og tidssone er riktig!';
+$lang['date_automatic_synchronize'] = 'automatisk Synkroniser';
+$lang['date_date'] = 'Dato';
+$lang['date_synchronize'] = 'synkron~~POS=TRUNC';
+$lang['date_synchronize_now'] = 'synkron~~POS=TRUNC nå';
+$lang['date_synchronize_wizard_tip'] = 'Med automatisk synkronisere aktivert, vil systemet synkronisere sin klokke fra tid til annen.';
+$lang['date_synchronized'] = 'synkronisert';
+$lang['date_synchronizing'] = 'synkronisering';
+$lang['date_time'] = 'Tid';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'Tid serveren er ugyldig.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Tidssynkronisering planen er ugyldig.';
+$lang['date_time_zone'] = 'Tidssone';
+$lang['date_time_zone_is_invalid'] = 'Tidssone er ugyldig.';
+$lang['date_time_zone_wizard_help'] = 'Mange applikasjoner og tjenester krever riktig tidssone og en nøyaktig klokke, så det er viktig at den er riktig innstilt.';

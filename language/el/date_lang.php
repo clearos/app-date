a6e5c5d35b6aa8371944d4bbf493b1e2f27fb6d3
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 22:32:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Η ζώνη ώρας και εργαλείο συγχρονισμού του ρολογιού.';
+$lang['date_app_name'] = 'Ημερομηνία και ώρα';
+$lang['date_app_tooltip'] = 'VoIP, υπηρεσίες ελέγχου ταυτότητας και άλλες εφαρμογές απαιτούν σωστές ρυθμίσεις του χρόνου, γι 'αυτό παρακαλώ βεβαιωθείτε ότι το ρολόι σας και τη ζώνη ώρας είναι σωστές!';
+$lang['date_automatic_synchronize'] = 'αυτόματη Συγχρονισμός';
+$lang['date_date'] = 'Ημερομηνία';
+$lang['date_synchronize'] = 'Συγχρονίζω';
+$lang['date_synchronize_now'] = 'Συγχρονίστε Τώρα';
+$lang['date_synchronize_wizard_tip'] = 'Με την αυτόματη συγχρονισμό ενεργοποιημένη, το σύστημα θα συγχρονίσετε το ρολόι του από καιρό σε καιρό.';
+$lang['date_synchronized'] = 'συγχρονισμένη';
+$lang['date_synchronizing'] = 'Συγχρονισμός';
+$lang['date_time'] = 'χρόνος';
+$lang['date_time_server'] = 'Ώρα Σέρβερ';
+$lang['date_time_server_is_invalid'] = 'Ώρα διακομιστή δεν είναι έγκυρο.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'χρονοδιάγραμμα συγχρονισμού ώρας δεν είναι έγκυρο.';
+$lang['date_time_zone'] = 'Ζώνη ώρας';
+$lang['date_time_zone_is_invalid'] = 'ζώνη ώρας είναι άκυρη.';
+$lang['date_time_zone_wizard_help'] = 'Πολλές εφαρμογές και υπηρεσίες απαιτούν σωστές πληροφορίες ζώνης ώρας και ένα ακριβές ρολόι, έτσι είναι σημαντικό ότι έχει ρυθμιστεί σωστά.';

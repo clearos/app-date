c99cf3c598c75d457b97d9650b351142dd2494d6
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 21:44:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'დროის ზონაში და საათის სინქრონიზაცია ინსტრუმენტი.';
+$lang['date_app_name'] = 'თარიღი და დრო';
+$lang['date_app_tooltip'] = 'VoIP, ავტორიზაციის მომსახურების და სხვა პროგრამები მოითხოვს სათანადო დროის პარამეტრების ასე რომ გთხოვთ დარწმუნდით, რომ თქვენი საათი და დროის ზონაში სწორია!';
+$lang['date_automatic_synchronize'] = 'ავტომატური სინქრონიზაცია';
+$lang['date_date'] = 'თარიღი';
+$lang['date_synchronize'] = 'სინქრონიზაცია';
+$lang['date_synchronize_now'] = 'სინქრონიზაცია ახლა';
+$lang['date_synchronize_wizard_tip'] = 'ავტომატური სინქრონიზაცია ჩართულია, სისტემა სინქრონიზაციისათვის მისი საათის დროდადრო.';
+$lang['date_synchronized'] = 'სინქრონული';
+$lang['date_synchronizing'] = 'Synchronizing';
+$lang['date_time'] = 'დრო';
+$lang['date_time_server'] = 'ახლა სერვერი';
+$lang['date_time_server_is_invalid'] = 'ახლა სერვერის არასწორია.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'დროის სინქრონიზაციის გრაფიკი არასწორია.';
+$lang['date_time_zone'] = 'დროის სარტყელი';
+$lang['date_time_zone_is_invalid'] = 'დროის ზონაში არასწორია.';
+$lang['date_time_zone_wizard_help'] = 'ბევრი apps და მომსახურების მოითხოვს სწორი დრო ზონაში ინფორმაცია და ზუსტი საათი, ამიტომ მნიშვნელოვანია, რომ ეს არის სწორად.';

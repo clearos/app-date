ec57002a3e9db5f0a25a50a8541130aba70c205a
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 18:43:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'वेळ क्षेत्र आणि घड्याळ समक्रमण साधन.';
+$lang['date_app_name'] = 'तारीख आणि वेळ';
+$lang['date_app_tooltip'] = 'जो VoIP, प्रमाणीकरण सेवा आणि इतर अनुप्रयोग, योग्य वेळ सेटिंग आवश्यक आहे म्हणून कृपया घड्याळ आणि टाइम झोन योग्य आहेत याची खात्री करा!';
+$lang['date_automatic_synchronize'] = 'स्वयंचलित समजुळणी';
+$lang['date_date'] = 'तारीख';
+$lang['date_synchronize'] = 'समक्रमित करा';
+$lang['date_synchronize_now'] = 'आता समक्रमित करा';
+$lang['date_synchronize_wizard_tip'] = 'स्वयंचलित समजुळणी सक्षम करून, प्रणाली वेळोवेळी त्याचे घड्याळ समक्रमित करेल.';
+$lang['date_synchronized'] = 'समक्रमित';
+$lang['date_synchronizing'] = 'समक्रमण';
+$lang['date_time'] = 'वेळ';
+$lang['date_time_server'] = 'वेळ सर्व्हर';
+$lang['date_time_server_is_invalid'] = 'वेळ सर्व्हर अवैध आहे.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'वेळ समजुळवणी वेळापत्रक अवैध आहे.';
+$lang['date_time_zone'] = 'वेळ क्षेत्र';
+$lang['date_time_zone_is_invalid'] = 'वेळ क्षेत्र अवैध आहे.';
+$lang['date_time_zone_wizard_help'] = 'अनेक अॅप्स आणि सेवा योग्य वेळ क्षेत्र माहिती आणि अचूक घड्याळ आवश्यक आहे, त्यामुळे तो योग्य प्रकारे सेट केले महत्वाचे आहे.';

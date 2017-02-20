4b1bc23e73efff305fb65ab36857af6f9c677844
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-20 22:37:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Die Zeitzone und Taktsynchronisation Werkzeug.';
+$lang['date_app_name'] = 'Datum und Uhrzeit';
+$lang['date_app_tooltip'] = 'VoIP, Authentifizierungsdienste und andere Anwendungen erfordern eine ausreichende Zeiteinstellungen, also bitte Ihre Uhr und Zeitzone stellen Sie sicher, korrekt sind!';
+$lang['date_automatic_synchronize'] = 'Automatische Synchronisierung';
+$lang['date_date'] = 'Datum';
+$lang['date_synchronize'] = 'Synchronisieren';
+$lang['date_synchronize_now'] = 'Jetzt synchronisieren';
+$lang['date_synchronize_wizard_tip'] = 'Bei der automatischen Synchronisieren aktiviert ist, wird das System seine Uhr von Zeit zu Zeit zu synchronisieren.';
+$lang['date_synchronized'] = 'Synchronisierte';
+$lang['date_synchronizing'] = 'Synchronisieren';
+$lang['date_time'] = 'Zeit';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'Zeitserver ist ungültig.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Zeitsynchronisation Zeitplan ist ungültig.';
+$lang['date_time_zone'] = 'Zeitzone';
+$lang['date_time_zone_is_invalid'] = 'Zeitzone ist ungültig.';
+$lang['date_time_zone_wizard_help'] = 'Viele Anwendungen und Dienste erfordern korrekte Zeitzoneninformationen und eine genaue Uhr, so ist es wichtig, dass sie richtig eingestellt ist.';

271c87a2b6cfc92e3fa04cdedb34464b99c4206e
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-21 17:25:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Vremenska zona i sat sinkronizacija alat.';
+$lang['date_app_name'] = 'Datum i vrijeme';
+$lang['date_app_tooltip'] = 'VoIP, autentikaciju usluge i druge aplikacije zahtijevaju odgovarajuće postavke vremena, pa vas molimo da provjerite je li vaš sat i vremenska zona ispravni!';
+$lang['date_automatic_synchronize'] = 'Automatski Sinkronizacija';
+$lang['date_date'] = 'Datum';
+$lang['date_synchronize'] = 'Sinkronizacija';
+$lang['date_synchronize_now'] = 'Sinkronizacija Sad';
+$lang['date_synchronize_wizard_tip'] = 'S omogućeno automatsko usklađivanje, sustav će uskladiti svoj sat s vremena na vrijeme.';
+$lang['date_synchronized'] = 'sinkronizirano';
+$lang['date_synchronizing'] = 'Sinkronizacija';
+$lang['date_time'] = 'Vrijeme';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'Vrijeme poslužitelj je nevažeći.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Raspored sinkronizacije vremena je nevažeći.';
+$lang['date_time_zone'] = 'Vremenska zona';
+$lang['date_time_zone_is_invalid'] = 'Vremenska zona je nevažeći.';
+$lang['date_time_zone_wizard_help'] = 'Mnoge aplikacije i usluge zahtijevaju točne podatke vremensku zonu i točan sat, tako da je važno da je ispravno postavljena.';

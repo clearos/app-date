09e2cd50abc5f120199b4e409cd167d65b9db26a
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-21 17:39:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Гадзінны пояс і інструмент сінхранізацыі гадзін.';
+$lang['date_app_name'] = 'Дата і час';
+$lang['date_app_tooltip'] = 'VoIP, паслугі аўтэнтыфікацыі і іншыя прыкладання патрабуюць наладзіць тэлефон часу, таму пераканайцеся, што вашыя гадзіны і гадзінны пояс правільна!';
+$lang['date_automatic_synchronize'] = 'аўтаматычная сінхранізацыя';
+$lang['date_date'] = 'дата';
+$lang['date_synchronize'] = 'сінхранізаваць';
+$lang['date_synchronize_now'] = 'сінхранізаваць зараз';
+$lang['date_synchronize_wizard_tip'] = 'З уключанай функцыяй аўтаматычнай сінхранізацыі, то сістэма будзе сінхранізаваць свой гадзіннік, час ад часу.';
+$lang['date_synchronized'] = 'сінхранізаваны';
+$lang['date_synchronizing'] = 'сінхранізуецца';
+$lang['date_time'] = 'час';
+$lang['date_time_server'] = 'час сервера';
+$lang['date_time_server_is_invalid'] = 'Сервер часу з&#39;яўляецца несапраўдным.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Графік сінхранізацыі часу з&#39;яўляецца несапраўдным.';
+$lang['date_time_zone'] = 'гадзінны пояс';
+$lang['date_time_zone_is_invalid'] = 'Несапраўдны гадзінны пояс.';
+$lang['date_time_zone_wizard_help'] = 'Многія прыкладання і паслугі патрабуюць правільнай інфармацыі аб часавым поясе і дакладныя гадзіны, таму вельмі важна, што ён усталяваны правільна.';

c1c7a159cef2a8ca969b08be3012cc1d8ed0578b
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-21 17:57:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Die tydsone en klok sinchronisasie hulpmiddel.';
+$lang['date_app_name'] = 'Datum en tyd';
+$lang['date_app_tooltip'] = 'VoIP, verifikasie dienste en ander programme vereis behoorlike tyd instellings, so maak asseblief seker dat jou horlosie en tydsone korrek is!';
+$lang['date_automatic_synchronize'] = 'outomatiese Synchroniseer';
+$lang['date_date'] = 'datum';
+$lang['date_synchronize'] = 'sinchroniseer';
+$lang['date_synchronize_now'] = 'sinchroniseer Nou';
+$lang['date_synchronize_wizard_tip'] = 'Met outomatiese sinchroniseer geaktiveer is, sal die stelsel die klok van tyd tot tyd te sinchroniseer.';
+$lang['date_synchronized'] = 'gesinchroniseer';
+$lang['date_synchronizing'] = 'Synchroniseren';
+$lang['date_time'] = 'tyd';
+$lang['date_time_server'] = 'tyd Server';
+$lang['date_time_server_is_invalid'] = 'Tyd bediener is ongeldig.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Tyd sinchronisasie skedule is ongeldig.';
+$lang['date_time_zone'] = 'Tydsone';
+$lang['date_time_zone_is_invalid'] = 'Tydsone is ongeldig.';
+$lang['date_time_zone_wizard_help'] = 'Baie programme en dienste benodig korrekte tydsone inligting en &#39;n akkurate horlosie, daarom is dit belangrik dat dit korrek is ingestel.';

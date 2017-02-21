d0e3157ac7a192e457ff343401475d1df0df6aa9
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-21 17:27:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = '时区和时钟同步工具。';
+$lang['date_app_name'] = '日期和时间';
+$lang['date_app_tooltip'] = 'VoIP的认证服务和其他应用程序需要适当的时间设置，所以请确保您的时钟和时区是正确的！';
+$lang['date_automatic_synchronize'] = '自动同步';
+$lang['date_date'] = '日期';
+$lang['date_synchronize'] = '同步';
+$lang['date_synchronize_now'] = '立即同步';
+$lang['date_synchronize_wizard_tip'] = '与启用了自动同步时，系统将它的时钟同步不时。';
+$lang['date_synchronized'] = '同步';
+$lang['date_synchronizing'] = '同步';
+$lang['date_time'] = '时间';
+$lang['date_time_server'] = '时间服务器';
+$lang['date_time_server_is_invalid'] = '时间服务器是无效的。';
+$lang['date_time_synchronization_schedule_is_invalid'] = '时间同步日程安排是无效的。';
+$lang['date_time_zone'] = '时区';
+$lang['date_time_zone_is_invalid'] = '时区是无效的。';
+$lang['date_time_zone_wizard_help'] = '许多应用程序和服务需要正确的时区信息和准确的时钟，使其正确设置是很重要的。';

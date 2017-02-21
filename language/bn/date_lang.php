3c16e494c9f247c8c635f458559b4a47fcb1a9e0
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-21 17:37:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'সময় জোন এবং ঘড়ি সুসংগতি টুল.';
+$lang['date_app_name'] = 'তারিখ এবং সময়';
+$lang['date_app_tooltip'] = 'ভিওআইপি, প্রমাণীকরণ সেবা ও অন্যান্য অ্যাপ্লিকেশন, সঠিক সময় সেটিংস প্রয়োজন তাই নিশ্চিত আপনার ঘড়ি এবং সময় জোন সঠিক কিনা তা নিশ্চিত করুন!';
+$lang['date_automatic_synchronize'] = 'স্বয়ংক্রিয় সিংক্রোনাইজ';
+$lang['date_date'] = 'তারিখ';
+$lang['date_synchronize'] = 'সময়ের সামঁজস্যবিধান করা';
+$lang['date_synchronize_now'] = 'এখন সিংক্রোনাইজ করুন';
+$lang['date_synchronize_wizard_tip'] = 'স্বয়ংক্রিয় সিংক্রোনাইজ সক্ষম থাকলে, সিস্টেমের সময় থেকে সময় তার ঘড়ি সুসংগত করা হবে.';
+$lang['date_synchronized'] = 'সিঙ্ক্রোনাইজ করা';
+$lang['date_synchronizing'] = 'সুসংগতি';
+$lang['date_time'] = 'সময়';
+$lang['date_time_server'] = 'সময় সার্ভার';
+$lang['date_time_server_is_invalid'] = 'সময় সার্ভার অবৈধ.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'সময় সুসংগতি সময়সূচী অবৈধ.';
+$lang['date_time_zone'] = 'সময় অঞ্চল';
+$lang['date_time_zone_is_invalid'] = 'টাইম জোন অবৈধ.';
+$lang['date_time_zone_wizard_help'] = 'অনেক অ্যাপ্লিকেশন এবং সেবা সঠিক সময় জোন তথ্য এবং একটি সঠিক ঘড়ি প্রয়োজন, তাই এটা গুরুত্বপূর্ণ এটি সঠিকভাবে সেট করা হয় যে.';

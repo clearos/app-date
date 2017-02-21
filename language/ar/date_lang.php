70be78744bae2563e44e0771ecc1b3edc1f7250f
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-21 17:51:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'المنطقة الزمنية و أداة تزامن ساعة.';
+$lang['date_app_name'] = 'التاريخ و الوقت';
+$lang['date_app_tooltip'] = 'الصوت عبر بروتوكول الإنترنت، وخدمات التوثيق وغيرها من التطبيقات تتطلب إعدادات الوقت المناسب، لذا يرجى التأكد من الساعة والمنطقة الزمنية الصحيحة!';
+$lang['date_automatic_synchronize'] = 'تزامن تلقائي';
+$lang['date_date'] = 'تاريخ';
+$lang['date_synchronize'] = 'تزامن';
+$lang['date_synchronize_now'] = 'مزامنة الآن';
+$lang['date_synchronize_wizard_tip'] = 'مع تزامن التلقائي تمكين، فإن نظام مزامنة ساعة من وقت لآخر.';
+$lang['date_synchronized'] = 'تزامن';
+$lang['date_synchronizing'] = 'مزامنة';
+$lang['date_time'] = 'زمن';
+$lang['date_time_server'] = 'خادم الوقت';
+$lang['date_time_server_is_invalid'] = 'خادم الوقت غير صالح.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'جدول مزامنة الوقت غير صالح.';
+$lang['date_time_zone'] = 'وحدة زمنية';
+$lang['date_time_zone_is_invalid'] = 'منطقة زمنية غير صالحة.';
+$lang['date_time_zone_wizard_help'] = 'تتطلب العديد من التطبيقات والخدمات الصحيحة معلومات المنطقة الزمنية وساعة دقيقة، لذلك من المهم أن يتم تعيين بشكل صحيح.';

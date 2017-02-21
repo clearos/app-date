296af3fad1812620053dce176ea43ed5246e2391
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-21 17:47:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Ժամային գոտին եւ ժամացույցը համաժամացման գործիք.';
+$lang['date_app_name'] = 'Ամսաթիվ եւ ժամ';
+$lang['date_app_tooltip'] = 'VoIP, իսկությունը ծառայություններ եւ այլ ծրագրերը պահանջում Ճիշտ ժամանակին պարամետրերը, ուստի խնդրում ենք համոզվեք, որ Ձեր ժամացույցը եւ ժամային գոտին ճիշտ են:';
+$lang['date_automatic_synchronize'] = 'Ավտոմատ Սինքրոնացրեք';
+$lang['date_date'] = 'ամսաթիվ';
+$lang['date_synchronize'] = 'համաժամանակացնել';
+$lang['date_synchronize_now'] = 'Սինքրոնացրեք Now';
+$lang['date_synchronize_wizard_tip'] = 'Ավտոմատ Սինքրոնացրեք հնարավորություն, որ համակարգը կլինի համեմատել իր ժամացույցը ժամանակ առ ժամանակ.';
+$lang['date_synchronized'] = 'սինխրոն';
+$lang['date_synchronizing'] = 'Synchronizing';
+$lang['date_time'] = 'ժամանակ';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'Ժամանակն սերվերը անվավեր:';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Ժամանակն համաժամացման ժամանակացույցը անվավեր է:';
+$lang['date_time_zone'] = 'Ժամային գոտի';
+$lang['date_time_zone_is_invalid'] = 'Ժամային գոտի է անվավեր:';
+$lang['date_time_zone_wizard_help'] = 'Շատ հավելվածներ ու ծառայություններ պահանջում ճշգրիտ ժամային գոտին, տեղեկություններ եւ ճշգրիտ ժամացույցը, ուստի կարեւոր է, որ այն սահմանվել է ճիշտ:';

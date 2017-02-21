b633459758c668cb971e36dd6e8c99892ab812fe
Apache <apache@ip-172-31-30-66.us-west-2.compute.internal>
2017-02-21 17:19:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/date_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['date_app_description'] = 'Časové pásmo a synchronizácia času nástroj.';
+$lang['date_app_name'] = 'Dátum a čas';
+$lang['date_app_tooltip'] = 'VoIP, služby overovania a ďalšie aplikácie vyžadujú správne nastavenie času, takže uistite sa, že vaše hodiny a časové pásmo sú správne!';
+$lang['date_automatic_synchronize'] = 'Automatická synchronizácia';
+$lang['date_date'] = 'dátum';
+$lang['date_synchronize'] = 'synchronizovať';
+$lang['date_synchronize_now'] = 'synchronizácia teraz';
+$lang['date_synchronize_wizard_tip'] = 'Pri zapnutej automatickej synchronizácie, systém bude synchronizovať svoje hodiny z času na čas.';
+$lang['date_synchronized'] = 'synchronizované';
+$lang['date_synchronizing'] = 'Synchronizácia';
+$lang['date_time'] = 'čas';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'Čas servera je neplatný.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Synchronizácia časový harmonogram je neplatný.';
+$lang['date_time_zone'] = 'Časové pásmo';
+$lang['date_time_zone_is_invalid'] = 'Časové pásmo je neplatný.';
+$lang['date_time_zone_wizard_help'] = 'Mnoho aplikácií a služieb vyžadujú správne informácie o časovom pásme a presný čas, takže je dôležité, že je správne nastavený.';

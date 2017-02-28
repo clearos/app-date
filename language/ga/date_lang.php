20fe4e8584dc88109cde84426fe280ff14c56263
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 15:19:39 +0000

    Add new files and directories and commit changes to local repository

--- language/ga/date_lang.php
+++ language/ga/date_lang.php
@@ -1,19 +1,19 @@
 <?php
 
-$lang['date_app_description'] = 'An crios ama agus uirlis sioncrónaithe, clog.';
-$lang['date_app_name'] = 'Dáta agus Am';
-$lang['date_app_tooltip'] = 'VoIP, a cheangal ar na seirbhísí fíordheimhnithe agus iarratais eile suímh am cuí, le do thoil déan cinnte go bhfuil do clog agus crios ama ceart!';
-$lang['date_automatic_synchronize'] = 'Uathoibríoch Sioncrónaigh';
-$lang['date_date'] = 'dáta';
-$lang['date_synchronize'] = 'Sioncrónaigh';
-$lang['date_synchronize_now'] = 'Sioncrónaigh Anois';
-$lang['date_synchronize_wizard_tip'] = 'Le sioncrónú uathoibríoch chumas, beidh an córas sioncrónú a chloig ó am go ham.';
-$lang['date_synchronized'] = 'synchronized';
-$lang['date_synchronizing'] = 'synchronizing';
-$lang['date_time'] = 'Am';
-$lang['date_time_server'] = 'Am Freastalaí';
-$lang['date_time_server_is_invalid'] = 'Is freastalaí Am neamhbhailí.';
-$lang['date_time_synchronization_schedule_is_invalid'] = 'Is sceideal ama sioncrónaithe neamhbhailí.';
-$lang['date_time_zone'] = 'Crios Ama';
-$lang['date_time_zone_is_invalid'] = 'Is crios ama neamhbhailí.';
-$lang['date_time_zone_wizard_help'] = 'A cheangal go leor apps agus seirbhísí eolais crios ama cruinn agus clog cruinn, mar sin tá sé tábhachtach go bhfuil sé socraithe i gceart.';
+$lang['date_app_description'] = 'The time zone and clock synchronization tool.';
+$lang['date_app_name'] = 'Date and Time';
+$lang['date_app_tooltip'] = 'VoIP, authentication services and other applications require proper time settings, so please make sure your clock and time zone are correct!';
+$lang['date_automatic_synchronize'] = 'Automatic Synchronize';
+$lang['date_date'] = 'Date';
+$lang['date_synchronize'] = 'Synchronize';
+$lang['date_synchronize_now'] = 'Synchronize Now';
+$lang['date_synchronize_wizard_tip'] = 'With automatic synchronize enabled, the system will synchronize its clock from time to time.';
+$lang['date_synchronized'] = 'Synchronized';
+$lang['date_synchronizing'] = 'Synchronizing';
+$lang['date_time'] = 'Time';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'Time server is invalid.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Time synchronization schedule is invalid.';
+$lang['date_time_zone'] = 'Time Zone';
+$lang['date_time_zone_is_invalid'] = 'Time zone is invalid.';
+$lang['date_time_zone_wizard_help'] = 'Many apps and services require correct time zone information and an accurate clock, so it is important that it is set correctly.';

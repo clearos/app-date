4ae9ebed05ad7656de667760fdec37644b89e330
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 15:21:02 +0000

    Add new files and directories and commit changes to local repository

--- language/la/date_lang.php
+++ language/la/date_lang.php
@@ -1,19 +1,19 @@
 <?php
 
-$lang['date_app_description'] = 'Tempus zona horologium synchronization et instrumentum.';
-$lang['date_app_name'] = 'Dies et tempus';
-$lang['date_app_tooltip'] = 'VoIP, authenticas propria officia et usus simul Occasus, planto certus ut velit tempus zona horologium et vera sunt!';
+$lang['date_app_description'] = 'The time zone and clock synchronization tool.';
+$lang['date_app_name'] = 'Date and Time';
+$lang['date_app_tooltip'] = 'VoIP, authentication services and other applications require proper time settings, so please make sure your clock and time zone are correct!';
 $lang['date_automatic_synchronize'] = 'Automatic Synchronize';
 $lang['date_date'] = 'Date';
-$lang['date_synchronize'] = 'synchronize';
-$lang['date_synchronize_now'] = 'Sed synchronize';
-$lang['date_synchronize_wizard_tip'] = 'Automatic synchronize enabled, quod ratio dolor synchronize suum de tempore usque ad tempus.';
-$lang['date_synchronized'] = 'synchronized';
-$lang['date_synchronizing'] = 'synchronum';
-$lang['date_time'] = 'Tempus';
-$lang['date_time_server'] = 'Tempus Server';
-$lang['date_time_server_is_invalid'] = 'Servo tempus nullum est.';
-$lang['date_time_synchronization_schedule_is_invalid'] = 'Synchronization schedule tempus nullum est.';
-$lang['date_time_zone'] = 'Zona temporis';
-$lang['date_time_zone_is_invalid'] = 'Temporalis est invalidum.';
-$lang['date_time_zone_wizard_help'] = 'Multis apps, et servitia requirit rectam tempus zona horologium notitia, et diligenter, sic est maximus ut bene sit.';
+$lang['date_synchronize'] = 'Synchronize';
+$lang['date_synchronize_now'] = 'Synchronize Now';
+$lang['date_synchronize_wizard_tip'] = 'With automatic synchronize enabled, the system will synchronize its clock from time to time.';
+$lang['date_synchronized'] = 'Synchronized';
+$lang['date_synchronizing'] = 'Synchronizing';
+$lang['date_time'] = 'Time';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'Time server is invalid.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Time synchronization schedule is invalid.';
+$lang['date_time_zone'] = 'Time Zone';
+$lang['date_time_zone_is_invalid'] = 'Time zone is invalid.';
+$lang['date_time_zone_wizard_help'] = 'Many apps and services require correct time zone information and an accurate clock, so it is important that it is set correctly.';

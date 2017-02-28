43af4a8ea1bd5433c03a3d75929e3de8505dd85c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 15:21:10 +0000

    Add new files and directories and commit changes to local repository

--- language/lo/date_lang.php
+++ language/lo/date_lang.php
@@ -1,19 +1,19 @@
 <?php
 
-$lang['date_app_description'] = 'ເຂດທີ່ໃຊ້ເວລາແລະເຄື່ອງມືການຊິງໂຄໄນໂມງ.';
-$lang['date_app_name'] = 'ວັນແລະເວລາ';
-$lang['date_app_tooltip'] = 'VoIP, ການບໍລິການການກວດສອບແລະຄໍາຮ້ອງສະຫມັກອື່ນໆຮຽກຮ້ອງໃຫ້ມີການຕັ້ງຄ່າທີ່ໃຊ້ເວລາທີ່ເຫມາະສົມ, ສະນັ້ນກະລຸນາເຮັດໃຫ້ແນ່ໃຈວ່າໂມງແລະເຂດທີ່ໃຊ້ເວລາຂອງທ່ານແມ່ນຖືກຕ້ອງ!';
-$lang['date_automatic_synchronize'] = 'Synchronize ອັດຕະໂນມັດ';
-$lang['date_date'] = 'ວັນທີ່ສະຫມັກ';
-$lang['date_synchronize'] = 'ປະສານ';
-$lang['date_synchronize_now'] = 'ປະສານໃນປັດຈຸບັນ';
-$lang['date_synchronize_wizard_tip'] = 'ກັບ synchronize ອັດຕະໂນມັດເປີດໃຫ້ໃຊ້ງານ, ລະບົບຈະ synchronize ໂມງຂອງຕົນຈາກທີ່ໃຊ້ເວລາກັບທີ່ໃຊ້ເວລາ.';
-$lang['date_synchronized'] = 'ຍໍາ';
-$lang['date_synchronizing'] = 'ກົງກັນ';
-$lang['date_time'] = 'ທີ່ໃຊ້ເວລາ';
-$lang['date_time_server'] = 'ທີ່ໃຊ້ເວລາ Server';
-$lang['date_time_server_is_invalid'] = 'ເຄື່ອງແມ່ຂ່າຍທີ່ໃຊ້ເວລາແມ່ນບໍ່ຖືກຕ້ອງ.';
-$lang['date_time_synchronization_schedule_is_invalid'] = 'ກໍານົດເວລາ synchronization ທີ່ໃຊ້ເວລາແມ່ນບໍ່ຖືກຕ້ອງ.';
-$lang['date_time_zone'] = 'ເຂດ​ເວ​ລາ';
-$lang['date_time_zone_is_invalid'] = 'ເຂດທີ່ໃຊ້ເວລາແມ່ນບໍ່ຖືກຕ້ອງ.';
-$lang['date_time_zone_wizard_help'] = 'ກິດແລະການບໍລິການຈໍານວນຫຼາຍຮຽກຮ້ອງໃຫ້ມີຂໍ້ມູນເຂດທີ່ໃຊ້ເວລາທີ່ຖືກຕ້ອງແລະໂມງທີ່ຖືກຕ້ອງ, ສະນັ້ນມັນເປັນສິ່ງສໍາຄັນວ່າມັນໄດ້ຖືກກໍານົດຢ່າງຖືກຕ້ອງ.';
+$lang['date_app_description'] = 'The time zone and clock synchronization tool.';
+$lang['date_app_name'] = 'Date and Time';
+$lang['date_app_tooltip'] = 'VoIP, authentication services and other applications require proper time settings, so please make sure your clock and time zone are correct!';
+$lang['date_automatic_synchronize'] = 'Automatic Synchronize';
+$lang['date_date'] = 'Date';
+$lang['date_synchronize'] = 'Synchronize';
+$lang['date_synchronize_now'] = 'Synchronize Now';
+$lang['date_synchronize_wizard_tip'] = 'With automatic synchronize enabled, the system will synchronize its clock from time to time.';
+$lang['date_synchronized'] = 'Synchronized';
+$lang['date_synchronizing'] = 'Synchronizing';
+$lang['date_time'] = 'Time';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'Time server is invalid.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Time synchronization schedule is invalid.';
+$lang['date_time_zone'] = 'Time Zone';
+$lang['date_time_zone_is_invalid'] = 'Time zone is invalid.';
+$lang['date_time_zone_wizard_help'] = 'Many apps and services require correct time zone information and an accurate clock, so it is important that it is set correctly.';

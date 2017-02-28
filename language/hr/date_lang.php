c16abd83620fff9ea677635f83cce528c1f06b6b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 15:17:12 +0000

    Add new files and directories and commit changes to local repository

--- language/hr/date_lang.php
+++ language/hr/date_lang.php
@@ -1,19 +1,19 @@
 <?php
 
-$lang['date_app_description'] = 'Vremenska zona i sat sinkronizacija alat.';
-$lang['date_app_name'] = 'Datum i vrijeme';
-$lang['date_app_tooltip'] = 'VoIP, autentikaciju usluge i druge aplikacije zahtijevaju odgovarajuće postavke vremena, pa vas molimo da provjerite je li vaš sat i vremenska zona ispravni!';
-$lang['date_automatic_synchronize'] = 'Automatski Sinkronizacija';
-$lang['date_date'] = 'Datum';
-$lang['date_synchronize'] = 'Sinkronizacija';
-$lang['date_synchronize_now'] = 'Sinkronizacija Sad';
-$lang['date_synchronize_wizard_tip'] = 'S omogućeno automatsko usklađivanje, sustav će uskladiti svoj sat s vremena na vrijeme.';
-$lang['date_synchronized'] = 'sinkronizirano';
-$lang['date_synchronizing'] = 'Sinkronizacija';
-$lang['date_time'] = 'Vrijeme';
+$lang['date_app_description'] = 'The time zone and clock synchronization tool.';
+$lang['date_app_name'] = 'Date and Time';
+$lang['date_app_tooltip'] = 'VoIP, authentication services and other applications require proper time settings, so please make sure your clock and time zone are correct!';
+$lang['date_automatic_synchronize'] = 'Automatic Synchronize';
+$lang['date_date'] = 'Date';
+$lang['date_synchronize'] = 'Synchronize';
+$lang['date_synchronize_now'] = 'Synchronize Now';
+$lang['date_synchronize_wizard_tip'] = 'With automatic synchronize enabled, the system will synchronize its clock from time to time.';
+$lang['date_synchronized'] = 'Synchronized';
+$lang['date_synchronizing'] = 'Synchronizing';
+$lang['date_time'] = 'Time';
 $lang['date_time_server'] = 'Time Server';
-$lang['date_time_server_is_invalid'] = 'Vrijeme poslužitelj je nevažeći.';
-$lang['date_time_synchronization_schedule_is_invalid'] = 'Raspored sinkronizacije vremena je nevažeći.';
-$lang['date_time_zone'] = 'Vremenska zona';
-$lang['date_time_zone_is_invalid'] = 'Vremenska zona je nevažeći.';
-$lang['date_time_zone_wizard_help'] = 'Mnoge aplikacije i usluge zahtijevaju točne podatke vremensku zonu i točan sat, tako da je važno da je ispravno postavljena.';
+$lang['date_time_server_is_invalid'] = 'Time server is invalid.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Time synchronization schedule is invalid.';
+$lang['date_time_zone'] = 'Time Zone';
+$lang['date_time_zone_is_invalid'] = 'Time zone is invalid.';
+$lang['date_time_zone_wizard_help'] = 'Many apps and services require correct time zone information and an accurate clock, so it is important that it is set correctly.';

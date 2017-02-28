c6002b490c473cc67b25a7628cedd51c16f2b326
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 15:21:34 +0000

    Add new files and directories and commit changes to local repository

--- language/mg/date_lang.php
+++ language/mg/date_lang.php
@@ -1,19 +1,19 @@
 <?php
 
-$lang['date_app_description'] = 'The Fiozao ara-potoana sy ny famantaranandro synchronization fitaovana.';
-$lang['date_app_name'] = 'Daty sy ny ora';
-$lang['date_app_tooltip'] = 'VoIP, fanamarinana ny asa sy ny fampiharana hafa mitaky fotoana toe-javatra, koa miangavy ho azo antoka ny famantaranandro sy ny Fiozao ara-potoana no marina!';
+$lang['date_app_description'] = 'The time zone and clock synchronization tool.';
+$lang['date_app_name'] = 'Date and Time';
+$lang['date_app_tooltip'] = 'VoIP, authentication services and other applications require proper time settings, so please make sure your clock and time zone are correct!';
 $lang['date_automatic_synchronize'] = 'Automatic Synchronize';
 $lang['date_date'] = 'Date';
 $lang['date_synchronize'] = 'Synchronize';
-$lang['date_synchronize_now'] = 'Synchronize Ankehitriny';
-$lang['date_synchronize_wizard_tip'] = 'Miaraka mandeha ho azy synchronize afaka, ny rafitra dia synchronize ny famantaranandro ombieny ombieny.';
-$lang['date_synchronized'] = 'nampiarahina';
+$lang['date_synchronize_now'] = 'Synchronize Now';
+$lang['date_synchronize_wizard_tip'] = 'With automatic synchronize enabled, the system will synchronize its clock from time to time.';
+$lang['date_synchronized'] = 'Synchronized';
 $lang['date_synchronizing'] = 'Synchronizing';
 $lang['date_time'] = 'Time';
 $lang['date_time_server'] = 'Time Server';
-$lang['date_time_server_is_invalid'] = 'Time mpizara dia tsy manan-kery.';
-$lang['date_time_synchronization_schedule_is_invalid'] = 'Time synchronization fandaharam-potoana no tsy manan-kery.';
+$lang['date_time_server_is_invalid'] = 'Time server is invalid.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Time synchronization schedule is invalid.';
 $lang['date_time_zone'] = 'Time Zone';
-$lang['date_time_zone_is_invalid'] = 'Fiozao ara-potoana no tsy manan-kery.';
-$lang['date_time_zone_wizard_help'] = 'Maro Apps sy ny asa marina mitaky Fiozao ara-potoana vaovao sy marina tsara famantaranandro, noho izany dia zava-dehibe fa dia nametraka tsara.';
+$lang['date_time_zone_is_invalid'] = 'Time zone is invalid.';
+$lang['date_time_zone_wizard_help'] = 'Many apps and services require correct time zone information and an accurate clock, so it is important that it is set correctly.';

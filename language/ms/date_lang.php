3b6762641780d80eb44458da85da152f7429b822
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 15:22:24 +0000

    Add new files and directories and commit changes to local repository

--- language/ms/date_lang.php
+++ language/ms/date_lang.php
@@ -1,19 +1,19 @@
 <?php
 
-$lang['date_app_description'] = 'Zon masa dan alat penyegerakan jam.';
-$lang['date_app_name'] = 'Tarikh dan masa';
-$lang['date_app_tooltip'] = 'VoIP, perkhidmatan pengesahan dan aplikasi lain memerlukan tetapan masa yang tetap, jadi sila pastikan jam anda dan zon masa adalah betul!';
-$lang['date_automatic_synchronize'] = 'automatik Menyegerak';
-$lang['date_date'] = 'Tarikh';
-$lang['date_synchronize'] = 'Segerakkan';
-$lang['date_synchronize_now'] = 'Segerakkan sekarang';
-$lang['date_synchronize_wizard_tip'] = 'Dengan menyegerakkan automatik diaktifkan, sistem akan menyegerakkan jam dari semasa ke semasa.';
+$lang['date_app_description'] = 'The time zone and clock synchronization tool.';
+$lang['date_app_name'] = 'Date and Time';
+$lang['date_app_tooltip'] = 'VoIP, authentication services and other applications require proper time settings, so please make sure your clock and time zone are correct!';
+$lang['date_automatic_synchronize'] = 'Automatic Synchronize';
+$lang['date_date'] = 'Date';
+$lang['date_synchronize'] = 'Synchronize';
+$lang['date_synchronize_now'] = 'Synchronize Now';
+$lang['date_synchronize_wizard_tip'] = 'With automatic synchronize enabled, the system will synchronize its clock from time to time.';
 $lang['date_synchronized'] = 'Synchronized';
-$lang['date_synchronizing'] = 'Penyelarasan';
-$lang['date_time'] = 'Masa';
-$lang['date_time_server'] = 'masa Server';
-$lang['date_time_server_is_invalid'] = 'pelayan masa adalah tidak sah.';
-$lang['date_time_synchronization_schedule_is_invalid'] = 'jadual penyegerakan masa adalah tidak sah.';
-$lang['date_time_zone'] = 'Zon masa';
-$lang['date_time_zone_is_invalid'] = 'zon masa adalah tidak sah.';
-$lang['date_time_zone_wizard_help'] = 'Banyak aplikasi dan perkhidmatan memerlukan maklumat zon masa yang betul dan jam tepat, jadi ia adalah penting bahawa ia ditetapkan dengan betul.';
+$lang['date_synchronizing'] = 'Synchronizing';
+$lang['date_time'] = 'Time';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'Time server is invalid.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Time synchronization schedule is invalid.';
+$lang['date_time_zone'] = 'Time Zone';
+$lang['date_time_zone_is_invalid'] = 'Time zone is invalid.';
+$lang['date_time_zone_wizard_help'] = 'Many apps and services require correct time zone information and an accurate clock, so it is important that it is set correctly.';

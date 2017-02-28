ba84ef9427367dfd29b175916f1395c75a31f0d1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 15:15:21 +0000

    Add new files and directories and commit changes to local repository

--- language/sq/date_lang.php
+++ language/sq/date_lang.php
@@ -1,19 +1,19 @@
 <?php
 
-$lang['date_app_description'] = 'Zona Koha dhe mjet sinkronizimi clock.';
-$lang['date_app_name'] = 'Data dhe ora';
-$lang['date_app_tooltip'] = 'VoIP, shërbimet e origjinalitetit dhe kërkesat e tjera kërkojnë parametrat e duhura në kohë, kështu që ju lutemi sigurohuni ora juaj dhe zonës së kohës janë të sakta!';
-$lang['date_automatic_synchronize'] = 'automatike Synchronize';
-$lang['date_date'] = 'data';
-$lang['date_synchronize'] = 'sinkronizoj';
-$lang['date_synchronize_now'] = 'Synchronize Tani';
-$lang['date_synchronize_wizard_tip'] = 'Me sinkronizuar automatike aktivizuar, sistemi do të sinkronizuar orën e saj nga koha në kohë.';
-$lang['date_synchronized'] = 'sinkronizuar';
-$lang['date_synchronizing'] = 'Sinkronizimi';
-$lang['date_time'] = 'kohë';
-$lang['date_time_server'] = 'Koha Server';
-$lang['date_time_server_is_invalid'] = 'server koha është e pavlefshme.';
-$lang['date_time_synchronization_schedule_is_invalid'] = 'Orari sinkronizimi koha është e pavlefshme.';
+$lang['date_app_description'] = 'The time zone and clock synchronization tool.';
+$lang['date_app_name'] = 'Date and Time';
+$lang['date_app_tooltip'] = 'VoIP, authentication services and other applications require proper time settings, so please make sure your clock and time zone are correct!';
+$lang['date_automatic_synchronize'] = 'Automatic Synchronize';
+$lang['date_date'] = 'Date';
+$lang['date_synchronize'] = 'Synchronize';
+$lang['date_synchronize_now'] = 'Synchronize Now';
+$lang['date_synchronize_wizard_tip'] = 'With automatic synchronize enabled, the system will synchronize its clock from time to time.';
+$lang['date_synchronized'] = 'Synchronized';
+$lang['date_synchronizing'] = 'Synchronizing';
+$lang['date_time'] = 'Time';
+$lang['date_time_server'] = 'Time Server';
+$lang['date_time_server_is_invalid'] = 'Time server is invalid.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Time synchronization schedule is invalid.';
 $lang['date_time_zone'] = 'Time Zone';
-$lang['date_time_zone_is_invalid'] = 'Zona kohore është e pavlefshme.';
-$lang['date_time_zone_wizard_help'] = 'Shumë aplikacione dhe shërbime të kërkojë informacion të saktë zonë kohë dhe një orë të saktë, kështu që është e rëndësishme që ajo është e saktë.';
+$lang['date_time_zone_is_invalid'] = 'Time zone is invalid.';
+$lang['date_time_zone_wizard_help'] = 'Many apps and services require correct time zone information and an accurate clock, so it is important that it is set correctly.';

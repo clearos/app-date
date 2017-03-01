67b266aa2dc0e755a69a69976cbda6e324b8504b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 03:39:15 +0000

    Add new files and directories and commit changes to local repository

--- language/ny/date_lang.php
+++ language/ny/date_lang.php
@@ -1,19 +1,19 @@
 <?php
 
-$lang['date_app_description'] = 'The time zone and clock synchronization tool.';
-$lang['date_app_name'] = 'Date and Time';
-$lang['date_app_tooltip'] = 'VoIP, authentication services and other applications require proper time settings, so please make sure your clock and time zone are correct!';
-$lang['date_automatic_synchronize'] = 'Automatic Synchronize';
+$lang['date_app_description'] = 'The zone nthawi ndi chida koloko kalunzanitsidwe.';
+$lang['date_app_name'] = 'Date ndi Time';
+$lang['date_app_tooltip'] = 'VoIP, misonkhano kutsimikizika ndi ntchito zina amafuna zoikamo bwino nthawi, kotero chonde onetsetsani wotchi yanu ndi zone nthawi zoona!';
+$lang['date_automatic_synchronize'] = 'Makinawa synchronize';
 $lang['date_date'] = 'Date';
-$lang['date_synchronize'] = 'Synchronize';
-$lang['date_synchronize_now'] = 'Synchronize Now';
-$lang['date_synchronize_wizard_tip'] = 'With automatic synchronize enabled, the system will synchronize its clock from time to time.';
-$lang['date_synchronized'] = 'Synchronized';
+$lang['date_synchronize'] = 'synchronize';
+$lang['date_synchronize_now'] = 'synchronize Tsopano';
+$lang['date_synchronize_wizard_tip'] = 'Ndi zodziwikiratu synchronize chinathandiza, dongosolo adzakhala synchronize koloko yake nthawi ndi nthawi.';
+$lang['date_synchronized'] = 'zinkayendera';
 $lang['date_synchronizing'] = 'Synchronizing';
 $lang['date_time'] = 'Time';
 $lang['date_time_server'] = 'Time Server';
-$lang['date_time_server_is_invalid'] = 'Time server is invalid.';
-$lang['date_time_synchronization_schedule_is_invalid'] = 'Time synchronization schedule is invalid.';
+$lang['date_time_server_is_invalid'] = 'Time Seva ndi opanda pake.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Time kalunzanitsidwe ndandanda ili udzakhala osazindikilika.';
 $lang['date_time_zone'] = 'Time Zone';
-$lang['date_time_zone_is_invalid'] = 'Time zone is invalid.';
-$lang['date_time_zone_wizard_help'] = 'Many apps and services require correct time zone information and an accurate clock, so it is important that it is set correctly.';
+$lang['date_time_zone_is_invalid'] = 'Time zone ndi opanda pake.';
+$lang['date_time_zone_wizard_help'] = 'mapulogalamu ndi ntchito zambiri amafuna olondola nthawi zone mfundo ndi wotchi yolondola, choncho n&#39;kofunika kuti anapereka molondola.';

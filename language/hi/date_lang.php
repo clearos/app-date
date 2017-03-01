fa2f48b32d904219ba5a8fc09766b506b29f1051
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 03:59:24 +0000

    Add new files and directories and commit changes to local repository

--- language/hi/date_lang.php
+++ language/hi/date_lang.php
@@ -1,19 +1,19 @@
 <?php
 
-$lang['date_app_description'] = 'The time zone and clock synchronization tool.';
-$lang['date_app_name'] = 'Date and Time';
-$lang['date_app_tooltip'] = 'VoIP, authentication services and other applications require proper time settings, so please make sure your clock and time zone are correct!';
-$lang['date_automatic_synchronize'] = 'Automatic Synchronize';
-$lang['date_date'] = 'Date';
-$lang['date_synchronize'] = 'Synchronize';
-$lang['date_synchronize_now'] = 'Synchronize Now';
-$lang['date_synchronize_wizard_tip'] = 'With automatic synchronize enabled, the system will synchronize its clock from time to time.';
-$lang['date_synchronized'] = 'Synchronized';
-$lang['date_synchronizing'] = 'Synchronizing';
-$lang['date_time'] = 'Time';
-$lang['date_time_server'] = 'Time Server';
-$lang['date_time_server_is_invalid'] = 'Time server is invalid.';
-$lang['date_time_synchronization_schedule_is_invalid'] = 'Time synchronization schedule is invalid.';
-$lang['date_time_zone'] = 'Time Zone';
-$lang['date_time_zone_is_invalid'] = 'Time zone is invalid.';
-$lang['date_time_zone_wizard_help'] = 'Many apps and services require correct time zone information and an accurate clock, so it is important that it is set correctly.';
+$lang['date_app_description'] = 'समय क्षेत्र और घड़ी तुल्यकालन उपकरण।';
+$lang['date_app_name'] = 'दिनांक और समय';
+$lang['date_app_tooltip'] = 'वीओआईपी, प्रमाणीकरण सेवाएं और अन्य अनुप्रयोगों, उचित समय सेटिंग्स की आवश्यकता होती है तो सुनिश्चित करें कि अपनी घड़ी और समय क्षेत्र सही हैं तो कृपया!';
+$lang['date_automatic_synchronize'] = 'स्वचालित सिंक्रनाइज़';
+$lang['date_date'] = 'तारीख';
+$lang['date_synchronize'] = 'सिंक्रनाइज़';
+$lang['date_synchronize_now'] = 'अब सिंक्रनाइज़';
+$lang['date_synchronize_wizard_tip'] = 'स्वत: सिंक्रनाइज़ सक्षम होने के साथ, सिस्टम को समय-समय पर अपनी घड़ी सिंक्रनाइज़ करेंगे।';
+$lang['date_synchronized'] = 'सिंक्रनाइज़';
+$lang['date_synchronizing'] = 'तुल्यकालन';
+$lang['date_time'] = 'पहर';
+$lang['date_time_server'] = 'समय सर्वर';
+$lang['date_time_server_is_invalid'] = 'समय सर्वर अमान्य है।';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'समय तुल्यकालन अनुसूची अमान्य है।';
+$lang['date_time_zone'] = 'समय क्षेत्र';
+$lang['date_time_zone_is_invalid'] = 'समय क्षेत्र अमान्य है।';
+$lang['date_time_zone_wizard_help'] = 'कई क्षुधा और सेवाओं के सही समय क्षेत्र की जानकारी और एक सटीक घड़ी की आवश्यकता होती है, इसलिए यह महत्वपूर्ण है कि यह ठीक से सेट है।';

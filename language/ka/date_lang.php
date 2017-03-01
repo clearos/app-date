df00842bc06be3018248119b3bb39a0f64898fd5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 04:09:42 +0000

    Add new files and directories and commit changes to local repository

--- language/ka/date_lang.php
+++ language/ka/date_lang.php
@@ -1,19 +1,19 @@
 <?php
 
-$lang['date_app_description'] = 'The time zone and clock synchronization tool.';
-$lang['date_app_name'] = 'Date and Time';
-$lang['date_app_tooltip'] = 'VoIP, authentication services and other applications require proper time settings, so please make sure your clock and time zone are correct!';
-$lang['date_automatic_synchronize'] = 'Automatic Synchronize';
-$lang['date_date'] = 'Date';
-$lang['date_synchronize'] = 'Synchronize';
-$lang['date_synchronize_now'] = 'Synchronize Now';
-$lang['date_synchronize_wizard_tip'] = 'With automatic synchronize enabled, the system will synchronize its clock from time to time.';
-$lang['date_synchronized'] = 'Synchronized';
+$lang['date_app_description'] = 'დროის ზონაში და საათის სინქრონიზაცია ინსტრუმენტი.';
+$lang['date_app_name'] = 'თარიღი და დრო';
+$lang['date_app_tooltip'] = 'VoIP, ავტორიზაციის მომსახურების და სხვა პროგრამები მოითხოვს სათანადო დროის პარამეტრების ასე რომ გთხოვთ დარწმუნდით, რომ თქვენი საათი და დროის ზონაში სწორია!';
+$lang['date_automatic_synchronize'] = 'ავტომატური სინქრონიზაცია';
+$lang['date_date'] = 'თარიღი';
+$lang['date_synchronize'] = 'სინქრონიზაცია';
+$lang['date_synchronize_now'] = 'სინქრონიზაცია ახლა';
+$lang['date_synchronize_wizard_tip'] = 'ავტომატური სინქრონიზაცია ჩართულია, სისტემა სინქრონიზაციისათვის მისი საათის დროდადრო.';
+$lang['date_synchronized'] = 'სინქრონული';
 $lang['date_synchronizing'] = 'Synchronizing';
-$lang['date_time'] = 'Time';
-$lang['date_time_server'] = 'Time Server';
-$lang['date_time_server_is_invalid'] = 'Time server is invalid.';
-$lang['date_time_synchronization_schedule_is_invalid'] = 'Time synchronization schedule is invalid.';
-$lang['date_time_zone'] = 'Time Zone';
-$lang['date_time_zone_is_invalid'] = 'Time zone is invalid.';
-$lang['date_time_zone_wizard_help'] = 'Many apps and services require correct time zone information and an accurate clock, so it is important that it is set correctly.';
+$lang['date_time'] = 'დრო';
+$lang['date_time_server'] = 'ახლა სერვერი';
+$lang['date_time_server_is_invalid'] = 'ახლა სერვერის არასწორია.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'დროის სინქრონიზაციის გრაფიკი არასწორია.';
+$lang['date_time_zone'] = 'დროის სარტყელი';
+$lang['date_time_zone_is_invalid'] = 'დროის ზონაში არასწორია.';
+$lang['date_time_zone_wizard_help'] = 'ბევრი apps და მომსახურების მოითხოვს სწორი დრო ზონაში ინფორმაცია და ზუსტი საათი, ამიტომ მნიშვნელოვანია, რომ ეს არის სწორად.';

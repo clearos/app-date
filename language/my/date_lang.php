137845feee2a47d16026b653d12aa6f4ccf53013
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 03:36:28 +0000

    Add new files and directories and commit changes to local repository

--- language/my/date_lang.php
+++ language/my/date_lang.php
@@ -1,19 +1,19 @@
 <?php
 
-$lang['date_app_description'] = 'The time zone and clock synchronization tool.';
-$lang['date_app_name'] = 'Date and Time';
-$lang['date_app_tooltip'] = 'VoIP, authentication services and other applications require proper time settings, so please make sure your clock and time zone are correct!';
-$lang['date_automatic_synchronize'] = 'Automatic Synchronize';
-$lang['date_date'] = 'Date';
-$lang['date_synchronize'] = 'Synchronize';
-$lang['date_synchronize_now'] = 'Synchronize Now';
-$lang['date_synchronize_wizard_tip'] = 'With automatic synchronize enabled, the system will synchronize its clock from time to time.';
-$lang['date_synchronized'] = 'Synchronized';
-$lang['date_synchronizing'] = 'Synchronizing';
-$lang['date_time'] = 'Time';
-$lang['date_time_server'] = 'Time Server';
-$lang['date_time_server_is_invalid'] = 'Time server is invalid.';
-$lang['date_time_synchronization_schedule_is_invalid'] = 'Time synchronization schedule is invalid.';
-$lang['date_time_zone'] = 'Time Zone';
-$lang['date_time_zone_is_invalid'] = 'Time zone is invalid.';
-$lang['date_time_zone_wizard_help'] = 'Many apps and services require correct time zone information and an accurate clock, so it is important that it is set correctly.';
+$lang['date_app_description'] = 'အဆိုပါအချိန်ဇုန်နှင့်နာရီ synchronize လုပ်ပေးတဲ့ကိရိယာတခုဖြစ်တယ်။';
+$lang['date_app_name'] = 'ရက်စွဲနှင့်အချိန်';
+$lang['date_app_tooltip'] = 'VoIP, authentication ကိုဝန်ဆောင်မှုများနှင့်အခြားအသုံးချသင့်လျော်သောအချိန် setting များကိုမလိုအပ်, ဒါကြောင့်သင့်ရဲ့နာရီနှင့်အချိန်ဇုန်မှန်ကန်သောဖြစ်ကြောင်းသေချာအောင်ကျေးဇူးပြုပြီး!';
+$lang['date_automatic_synchronize'] = 'automatic synchronous';
+$lang['date_date'] = 'နေ့စှဲ';
+$lang['date_synchronize'] = 'ထပ်တူကျအောင်';
+$lang['date_synchronize_now'] = 'အခုတော့ထပ်တူကျအောင်';
+$lang['date_synchronize_wizard_tip'] = 'အလိုအလျှောက် synchronous enabled နှင့်အတူစနစ်အခြိနျမှနျမှ၎င်း၏နာရီတွေကိုတပြိုင်တည်းအလုပ်လုပ်ပါလိမ့်မယ်။';
+$lang['date_synchronized'] = 'ညှိ';
+$lang['date_synchronizing'] = 'synchronous';
+$lang['date_time'] = 'အချိန်';
+$lang['date_time_server'] = 'အချိန် Server ကို';
+$lang['date_time_server_is_invalid'] = 'အချိန် server ကိုမမှန်ကန်ဘူး။';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'အချိန် synchronization အချိန်ဇယားမမှန်ကန်ဘူး။';
+$lang['date_time_zone'] = 'အချိန်ဇုန်';
+$lang['date_time_zone_is_invalid'] = 'အချိန်ဇုန်မမှန်ကန်ဘူး။';
+$lang['date_time_zone_wizard_help'] = 'အများစုက apps များနှင့်ဝန်ဆောင်မှုများကိုမှန်ကန်သောအချိန်ဇုန်သတင်းအချက်အလက်နှင့်တစ်ဦးတိကျမှန်ကန်နာရီလိုအပ်သည်, ဒါကြောင့်မှန်မှန်ကန်ကန်သတ်မှတ်ထားကြောင်းအရေးကြီးပါတယ်။';

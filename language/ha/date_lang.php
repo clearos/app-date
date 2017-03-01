8981bb84fc7f85bbc3ae02c1a76a13766bd3bcc1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 03:58:06 +0000

    Add new files and directories and commit changes to local repository

--- language/ha/date_lang.php
+++ language/ha/date_lang.php
@@ -1,19 +1,19 @@
 <?php
 
-$lang['date_app_description'] = 'The time zone and clock synchronization tool.';
-$lang['date_app_name'] = 'Date and Time';
-$lang['date_app_tooltip'] = 'VoIP, authentication services and other applications require proper time settings, so please make sure your clock and time zone are correct!';
-$lang['date_automatic_synchronize'] = 'Automatic Synchronize';
-$lang['date_date'] = 'Date';
-$lang['date_synchronize'] = 'Synchronize';
-$lang['date_synchronize_now'] = 'Synchronize Now';
-$lang['date_synchronize_wizard_tip'] = 'With automatic synchronize enabled, the system will synchronize its clock from time to time.';
-$lang['date_synchronized'] = 'Synchronized';
-$lang['date_synchronizing'] = 'Synchronizing';
+$lang['date_app_description'] = 'A lokacin da zone da agogo aiki tare da kayan aiki.';
+$lang['date_app_name'] = 'Kwanan wata da lokaci';
+$lang['date_app_tooltip'] = 'VoIP, Tantance kalmar sirri ayyuka da kuma sauran aikace-aikace na bukatar kari saituna, don haka don Allah ka tabbata ka agogo da kuma lokacin zone ne daidai!';
+$lang['date_automatic_synchronize'] = 'atomatik aiki tare';
+$lang['date_date'] = 'kwanan';
+$lang['date_synchronize'] = 'Haɗa aiki tare da';
+$lang['date_synchronize_now'] = 'Haɗa aiki tare da Now';
+$lang['date_synchronize_wizard_tip'] = 'Tare da m aiki tare sa, da tsarin za su aiki tare da agogo daga lokaci zuwa lokaci.';
+$lang['date_synchronized'] = 'aiki';
+$lang['date_synchronizing'] = 'Aiki tare';
 $lang['date_time'] = 'Time';
 $lang['date_time_server'] = 'Time Server';
-$lang['date_time_server_is_invalid'] = 'Time server is invalid.';
-$lang['date_time_synchronization_schedule_is_invalid'] = 'Time synchronization schedule is invalid.';
+$lang['date_time_server_is_invalid'] = 'Time uwar garken ba daidai ba ne.';
+$lang['date_time_synchronization_schedule_is_invalid'] = 'Time aiki tare jadawalin ba daidai ba ne.';
 $lang['date_time_zone'] = 'Time Zone';
-$lang['date_time_zone_is_invalid'] = 'Time zone is invalid.';
-$lang['date_time_zone_wizard_help'] = 'Many apps and services require correct time zone information and an accurate clock, so it is important that it is set correctly.';
+$lang['date_time_zone_is_invalid'] = 'Time zone ba daidai ba ne.';
+$lang['date_time_zone_wizard_help'] = 'Mutane da yawa apps da kuma ayyuka na bukatar daidai lokaci zone bayanai da kuma wani m agogo, don haka yana da muhimmanci cewa an saita daidai.';
